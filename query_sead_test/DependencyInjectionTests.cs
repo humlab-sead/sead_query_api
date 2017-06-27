d3193360c3a1fda4ecabf0742303162134b8dec4
Roger Mähler <roger.mahler@hotmail.com>
2017-06-27 16:52:36 +0200

    Test rename

---
+++ query_sead_test/DependencyInjectionTests.cs
@@ -0,0 +1,101 @@
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Diagnostics;
+using DataAccessPostgreSqlProvider;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Autofac;
+using QuerySeadDomain;
+using QuerySeadAPI;
+
+namespace QuerySeadTests.IoC {
+
+    public interface IDependent {
+        void DoSomeThingNice();
+    }
+
+    public class MyDependent : IDependent {
+        public void DoSomeThingNice()
+        {
+            Debug.Write("Hello World!");
+            Console.Write("HEJ");
+        }
+    }
+
+    public interface IMyController {
+        void CallMyDependent();
+    }
+
+    public class MyController : IMyController {
+
+        public IDependent Dependent { get; set; }
+
+        public void CallMyDependent()
+        {
+            Dependent.DoSomeThingNice();
+        }
+
+    }
+
+    public class MyController2 : MyController
+    {
+        public MyController2(IDependent dependent)
+        {
+            this.Dependent = dependent;
+        }
+    }
+
+    [TestClass]
+    public class IoCTests
+    {
+
+        [TestMethod]
+        public void TestResolveService()
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterInstance<IDependent>(new MyDependent());
+            var container = builder.Build();
+            using (var scope = container.BeginLifetimeScope()) {
+                var service = scope.Resolve<IDependent>();
+                service.DoSomeThingNice();
+            }
+        }
+
+        [TestMethod]
+        public void TestResolveDependent()
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterInstance<IDependent>(new MyDependent()); //.As<IDependent>();
+            builder.RegisterType<MyController2>().As<IMyController>();
+            var container = builder.Build();
+            using (var scope = container.BeginLifetimeScope()) {
+                var service = scope.Resolve<IMyController>();
+                service.CallMyDependent();
+            }
+        }
+
+        [TestMethod]
+        public void TestMemberInjection()
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterInstance<IDependent>(new MyDependent()); //.As<IDependent>();
+            builder.RegisterType<MyController>().As<IMyController>()
+                .OnActivating(e => { e.Instance.Dependent = e.Context.Resolve<IDependent>(); });
+
+            var container = builder.Build();
+            using (var scope = container.BeginLifetimeScope()) {
+                var service = scope.Resolve<IMyController>();
+                service.CallMyDependent();
+            }
+        }
+
+        [TestMethod]
+        public void TestCanRegisterDependencies()
+        {
+            var container = new DependencyService().Register(null);
+            Assert.IsNotNull(container);
+        }
+
+    }
+}
