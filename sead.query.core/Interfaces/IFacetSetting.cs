c4ed8cfe31429de59853b412a375aefbe4b7fc0c
Roger Mähler <roger.mahler@hotmail.com>
2019-09-09 10:22:12 +0200

    Refactor: Moved interfaces to individual files

---
+++ sead.query.core/Interfaces/IFacetSetting.cs
@@ -0,0 +1,12 @@
+﻿namespace SeadQueryCore
+{
+    public interface IFacetSetting
+    {
+        string DirectCountTable { get; set; }
+        string DirectCountColumn { get; set; }
+        string IndirectCountTable { get; set; }
+        string IndirectCountColumn { get; set; }
+        int ResultQueryLimit { get; set; }
+        bool CategoryNameFilter { get; set; }
+    }
+}
