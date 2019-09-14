3c5603bfaf294b9efb1e6cedd996f09593753a2c
roger-mahler <roger.mahler@hotmail.com>
2019-09-14 08:44:03 +0200

    Moved factory to file. Removed unused property AliasFacets

--- sead.query.core/Model/FacetGraph.cs
+++ sead.query.core/Model/FacetGraph.cs
@@ -16,7 +16,7 @@ namespace SeadQueryCore
     }
 
     public interface IFacetsGraph {
-        List<Facet> AliasFacets { get; }
+        //List<Facet> AliasFacets { get; }
         Dictionary<Tuple<string, string>, GraphEdge> Edges { get; }
         NodesDictI NodesIds { get; }
         NodesDictS Nodes { get; }
@@ -36,18 +36,21 @@ namespace SeadQueryCore
         public NodesDictS Nodes { get; set; }
         public NodesDictI NodesIds { get; set; }
         public Dictionary<Tuple<string, string>, GraphEdge> Edges { get; set; }
-        public List<Facet> AliasFacets { get; set; }
+        //public List<Facet> AliasFacets { get; set; }
         public Dictionary<string, string> AliasTables;
         public WeightDictionary Weights { get; set; }
 
         public FacetsGraph(NodesDictS nodes, List<GraphEdge> edges, List<Facet> aliasFacets)
         {
             Nodes = nodes;
+
             NodesIds = nodes.Values.ToDictionary(x => x.NodeId);
+
             Edges = edges.ToDictionary(z => z.Key);
-            AliasFacets = aliasFacets;
-            //AliasTables = aliasFacets.ToDictionary(x => x.TargetName, x => x.AliasName);
-            AliasTables = aliasFacets.ToDictionary(x => x.AliasName, x => x.TargetTableName);
+
+            //AliasFacets = aliasFacets ?? new List<Facet>();
+            AliasTables = (aliasFacets ?? new List<Facet>())
+                    .ToDictionary(x => x.AliasName, x => (x.TargetTable?.ObjectName ?? ""));
 
             Weights = edges.GroupBy(p => p.SourceNodeId, (key, g) => (key, g.ToDictionary(x => x.TargetNodeId, x => x.Weight)))
                 .ToDictionary(x => x.Item1, y => y.Item2);
@@ -100,74 +103,6 @@ namespace SeadQueryCore
             foreach (var edge in Edges)
                 sb.Append($"{edge.Value.SourceName};{edge.Value.TargetName};{edge.Value.Weight}\n");
             return sb.ToString();
-        }
-    }
-
-    public class FacetGraphFactory : IFacetGraphFactory
-    {
-        public IRepositoryRegistry Context { get; private set; }
-
-        public FacetGraphFactory(IRepositoryRegistry context)
-        {
-            Context = context;
-        }
-
-        public IFacetsGraph Build()
-        {
-            // FIXME! Edges where source_table = target_table?? Key=(tbl_dataset,tbl_dataset)
-            // Could it be that key must be "table_name.column_name"
-
-            var edges = Context.Edges.GetAll().ToList();
-            var nodes = Context.Nodes.GetAll().ToDictionary(x => x.TableName);
-
-            var aliasFacets = Context.Facets.FindThoseWithAlias().ToList();
-
-            nodes = AddAliasNodes(aliasFacets, nodes);
-            edges = AddAliasEdges(edges, nodes, aliasFacets);
-            edges = AddReverseEdges(edges);
-
-            return new FacetsGraph(nodes, edges, aliasFacets);
-        }
-
-        private NodesDictS AddAliasNodes(List<Facet> aliasFacets, NodesDictS nodes)
-        {
-            var aliases = GetAliasNodes(nodes, aliasFacets).ToList();
-            aliases.ForEach(z => nodes[z.TableName] = z);
-            return nodes;
-        }
-
-        private List<GraphNode> GetAliasNodes(NodesDictS nodes, List<Facet> aliasFacets)
-        {
-            int id = 10000;
-            return aliasFacets
-                .Select(z => new GraphNode() { NodeId = id++, TableName = z.AliasName })
-                .Where(z => !nodes.ContainsKey(z.TableName)).ToList();
-        }
-
-        private List<GraphEdge> AddAliasEdges(List<GraphEdge> edges, NodesDictS nodes, List<Facet> aliasFacets)
-        {
-            // Copy target tables relations for all alias facets...
-            foreach (var facet in aliasFacets)
-            {
-                // ...fetch all relations where target is a node...
-                var targetEdges = edges.Where(x => x.SourceName == facet.TargetTableName || x.TargetName == facet.TargetTableName);
-
-                // ...for each target relation, create a corresponding alias relation...
-                var aliasEdges = targetEdges.Select(z => z.Alias(nodes[facet.TargetTableName], nodes[facet.AliasName]));
-
-                // ...filter away edges that already exists...
-                aliasEdges = aliasEdges.Where(z => !edges.Exists(w => w.EqualAs(z)));
-
-                edges.AddRange(aliasEdges);
-            }
-            return edges;
-        }
-
-        private List<GraphEdge> AddReverseEdges(List<GraphEdge> edges)
-        {
-            var reverse = edges.Where(z => z.SourceNodeId != z.TargetNodeId).Select(x => x.Reverse()).ToList();
-            edges.AddRange(reverse);
-            return edges;
         }
     }
 }
