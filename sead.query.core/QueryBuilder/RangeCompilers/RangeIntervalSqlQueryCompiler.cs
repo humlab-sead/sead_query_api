3f9dcad64ebc5e824e6c9b12a54f196b19572004
roger-mahler <roger.mahler@hotmail.com>
2019-09-24 11:12:51 +0200

    Improved formatting.

--- sead.query.core/QueryBuilder/RangeCompilers/RangeIntervalSqlQueryCompiler.cs
+++ sead.query.core/QueryBuilder/RangeCompilers/RangeIntervalSqlQueryCompiler.cs
@@ -6,9 +6,13 @@ namespace SeadQueryCore
     {
         public string Compile(int interval, int min, int max, int interval_count)
         {
-            string sql = $"" +
-                $"(SELECT n::text || ' => ' || (n + {interval})::text, n, n + {interval} \n" +
-                $" FROM generate_series({min}, {max}, {interval}) as a(n) WHERE n < {max})\n";
+            string sql = $@"
+            (
+                SELECT n::text || ' => ' || (n + {interval})::text, n, n + {interval}
+                FROM generate_series({min}, {max}, {interval}) as a(n)
+                WHERE n < {max}
+            )";
+
             return sql;
         }
     }
