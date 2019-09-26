ef268a1470373278c108cfb5d2655496a9e98a70
roger-mahler <roger.mahler@hotmail.com>
2019-09-26 07:07:13 +0200

    Added docker build script

--- docker-build.sh
+++ docker-build.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
 
+ wget https://raw.githubusercontent.com/humlab-sead/sead_query_api/master/Dockerfile
+ wget https://raw.githubusercontent.com/humlab-sead/sead_query_api/master/docker-compose.yml
+ wget https://raw.githubusercontent.com/humlab-sead/sead_query_api/master/docker-compose.override.yml
+
+docker build --rm -f "Dockerfile" -t sead_query_api:latest ./conf
+
