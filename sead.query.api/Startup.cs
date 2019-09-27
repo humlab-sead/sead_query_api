66098e81c9c28dfa32aeabc5509fd2ef1220313e
roger-mahler <roger.mahler@hotmail.com>
2019-09-27 11:31:22 +0200

    Updated Startup

--- sead.query.api/Startup.cs
+++ sead.query.api/Startup.cs
@@ -17,71 +17,7 @@ namespace SeadQueryAPI {
 
     public class Startup
     {
-        public IConfigurationRoot Configuration { get; }
         public IContainer Container { get; private set; }
-        public IQueryBuilderSetting Options { get; private set; }
-
-        public Startup(IHostingEnvironment env)
-        {
-
-            // Development, Staging, and Production. If ASPNETCORE_ENVIRONMENT isn't set, it defaults to Production.
-
-            Configuration = new ConfigurationBuilder()
-                //.SetBasePath(env.ContentRootPath)  // Directory.GetCurrentDirectory()
-                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-                .AddJsonFile("hosting.json", optional: true)
-                .AddEnvironmentVariables()
-                .Build();
-
-            Options = Configuration
-                .GetSection("QueryBuilderSetting")
-                .Get<QueryBuilderSetting>();
-
-        }
-
-        public IServiceProvider ConfigureServices(IServiceCollection services)
-        {
-            // Configuration.GetSection("Logging")
-            services
-                .AddOptions()
-                .AddCors()
-                .AddMvc()
-                .AddJsonOptions(options => {
-                    var settings = options.SerializerSettings;
-                    // settings.DateFormatHandling = Newtonsoft.Json.DateFormatHandling.IsoDateFormat;
-                    var resolver = new SeadQueryAPI.Serializers.SeadQueryResolver();
-                    settings.ContractResolver = resolver as DefaultContractResolver;
-                 });
-
-            services.AddLogging(builder => builder.AddConsole());
-            //AddSwagger(services);
-
-            Container = new DependencyService().Register(services, Options);
-
-            return Container.Resolve<IServiceProvider>();
-        }
-
-
-        private static void AddSwagger(IServiceCollection services)
-        {
-            //See https://github.com/domaindrivendev/Swashbuckle
-            // Register the Swagger generator, defining one or more Swagger documents
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new Info
-                {
-                    Title = "SEAD Query API",
-                    Version = "v1",
-                    Description = "API used by the SEAD Query clientS",
-                    TermsOfService = "None"
-                });
-                string[] files = { "SeadQueryAPI.xml", "SeadQueryCore.xml" };
-                foreach (var file in files)
-                {
-                    c.IncludeXmlComments(Path.Combine(PlatformServices.Default.Application.ApplicationBasePath, file));
-                }
-            });
-        }
 
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory, IApplicationLifetime appLifetime)
         {
@@ -98,6 +34,59 @@ namespace SeadQueryAPI {
             appLifetime.ApplicationStopped.Register(() => this.Container.Dispose());
         }
 
+        public IServiceProvider ConfigureServices(IServiceCollection services)
+        {
+            services
+                .AddOptions()
+                .AddCors()
+                .AddMvc()
+                .AddJsonOptions(opts => {
+                    var settings = opts.SerializerSettings;
+                    var resolver = new SeadQueryAPI.Serializers.SeadQueryResolver();
+                    settings.ContractResolver = resolver as DefaultContractResolver;
+                 });
+
+            services.AddLogging(builder => builder.AddConsole());
+            //AddSwagger(services);
+
+            var options = GetOptions();
+            Container = new DependencyService().Register(services, options);
+
+            return Container.Resolve<IServiceProvider>();
+        }
+
+
+        private static void AddSwagger(IServiceCollection services)
+        {
+            //See https://github.com/domaindrivendev/Swashbuckle
+            // Register the Swagger generator, defining one or more Swagger documents
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info
+                {
+                    Title = "SEAD Query API",
+                    Version = "v1",
+                    Description = "API used by the SEAD Query clients",
+                    TermsOfService = "None"
+                });
+                string[] files = { "SeadQueryAPI.xml", "SeadQueryCore.xml" };
+                foreach (var file in files)
+                {
+                    c.IncludeXmlComments(Path.Combine(PlatformServices.Default.Application.ApplicationBasePath, file));
+                }
+            });
+        }
+
+        private QueryBuilderSetting GetOptions()
+        {
+            var options = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddEnvironmentVariables()
+                .Build()
+                .GetSection("QueryBuilderSetting")
+                .Get<QueryBuilderSetting>();
+            return options;
+        }
     }
 }
 
