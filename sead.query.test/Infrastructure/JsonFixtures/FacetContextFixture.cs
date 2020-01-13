cdfbec33a8942b12b8551b44368e16db0eef1873
roger-mahler <roger.mahler@hotmail.com>
2020-01-13 12:51:32 +0100

    Test fixture: refactor & updates

---
+++ sead.query.test/Infrastructure/JsonFixtures/FacetContextFixture.cs
@@ -0,0 +1,35 @@
+﻿using DataAccessPostgreSqlProvider;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using System;
+using System.Text;
+
+namespace SeadQueryTest.Infrastructure
+{
+    public class FacetContextFixture : IDisposable
+    {
+        public string DataFolder { get; }
+        public FacetContext FacetContext { get; private set; }
+
+        public FacetContextFixture(string folder)
+        {
+            DataFolder = folder;
+            var options = new DbContextOptionsBuilder<FacetContext>()
+                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+                .EnableSensitiveDataLogging()
+                .Options;
+            FacetContext = new FacetContext(options);
+            Seed();
+        }
+
+        protected virtual void Seed()
+        {
+            FacetContext.SaveChanges();
+        }
+
+        public void Dispose()
+        {
+            FacetContext.Dispose();
+        }
+    }
+}
--- sead.query.test/Infrastructure/JsonFixtures/FacetContextSeededByJson.cs
+++ sead.query.test/Infrastructure/JsonFixtures/FacetContextSeededByJson.cs
@@ -1,56 +1,20 @@
 ﻿using DataAccessPostgreSqlProvider;
-using Microsoft.EntityFrameworkCore;
-using Newtonsoft.Json;
 using SeadQueryCore;
 using SeadQueryTest.Infrastructure.Scaffolding;
-using System;
 using System.Collections.Generic;
-using System.Text;
 
 namespace SeadQueryTest.Infrastructure
 {
-    public class FacetContextFixture : IDisposable
+    public class FacetContextSeededByJson : FacetContextFixture
     {
-        public string DataFolder { get; }
-        public FacetContext FacetContext { get; private set; }
-
-        public FacetContextFixture(string folder)
-        {
-            DataFolder = folder;
-            var options = new DbContextOptionsBuilder<FacetContext>()
-                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
-                .EnableSensitiveDataLogging()
-                .Options;
-            FacetContext = new FacetContext(options);
-            Seed();
-        }
-
-        protected virtual void Seed()
-        {
-            FacetContext.SaveChanges();
-        }
-
-        public void Dispose()
-        {
-            FacetContext.Dispose();
-        }
-    }
-
-    public class FacetContextFixtureSeededByFolder : FacetContextFixture
-    {
-        public FacetContextFixtureSeededByFolder(string folder) : base(folder)
+        public FacetContextSeededByJson(string folder) : base(folder)
         {
         }
 
         protected override void Seed()
         {
-            JsonSerializer serializer = new JsonSerializer {
-                // serializer.Converters.Add(new JavaScriptDateTimeConverter());
-                NullValueHandling = NullValueHandling.Ignore
-            };
-
             var path = DataFolder;
-            var reader = new ScaffoldReader();
+            var reader = new JsonReaderService();
 
             List<FacetGroup> facetGroups = new List<FacetGroup>(reader.Deserialize<FacetGroup>(path));
             FacetContext.AddRange(facetGroups);
