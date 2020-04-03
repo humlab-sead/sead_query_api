03ab00f46739bd54bd650ac70629885b99ce84c2
Roger Mähler <roger.mahler@hotmail.com>
2020-04-03 22:46:29 +0200

    Added ignore of FK constraint + file rename

---
+++ sead.query.test/Infrastructure/Mocks/FacetContext/SqliteConnectionFactory.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Data.Sqlite;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Text;
+
+namespace SeadQueryTest.Mocks
+{
+    internal static class SqliteConnectionFactory
+    {
+        public static DbConnection Create()
+        { 
+            var connection = new SqliteConnection("DataSource=:memory:;Foreign Keys = False");
+            connection.Open();
+            return connection;
+        }
+
+        public static DbConnection CreateAndOpen()
+        {
+            var connection = Create();
+            connection.Open();
+            return connection;
+        }
+    }
+}
