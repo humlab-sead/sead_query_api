ce52c3447caeaac71d32896bd6bd0ef55cfddc31
Roger Mähler <roger.mahler@hotmail.com>
2020-04-03 22:58:58 +0200

    Added Json seeded FacetContext

---
+++ sead.query.test/Infrastructure/Mocks/FacetContext/JsonSeededFacetContext.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SeadQueryInfra;
+using SeadQueryTest.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SeadQueryTest.Mocks
+{
+    public class JsonSeededFacetContext : FacetContext
+    {
+        public string Folder { get; }
+
+        public JsonSeededFacetContext(DbContextOptions options, string folder) : this(options)
+        {
+            Folder = folder;
+        }
+
+        public JsonSeededFacetContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            var reader = new JsonReaderService(new IgnoreJsonAttributesResolver());
+            foreach (var type in ScaffoldUtility.GetModelTypes()) {
+                builder.Entity(type).HasData(
+                    Deserialize(type, reader).ToArray()
+                );
+            }
+        }
+
+        public IEnumerable<object> Deserialize(Type type, JsonReaderService reader)
+        {
+            return (IEnumerable<object>)typeof(JsonReaderService)
+                .GetMethod("Deserialize")
+                .MakeGenericMethod(type)
+                .Invoke(reader, new object[] { Folder });
+        }
+    }
+}
