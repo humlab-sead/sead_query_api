61f78fa4614c578e847736c0b864085893ed47d6
Roger Mähler <roger.mahler@hotmail.com>
2020-04-09 10:04:47 +0200

    Added shortcut properties

--- sead.query.core/Services/CategoryCount/CategoryCountItem.cs
+++ sead.query.core/Services/CategoryCount/CategoryCountItem.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
 
 namespace SeadQueryCore
 {
@@ -17,6 +18,34 @@ namespace SeadQueryCore
         public int? Count { get; set; }
         public List<decimal> Extent;
         //public Dictionary<EFacetPickType, decimal> CategoryValues;
+
+        [JsonIgnore]
+        public decimal Lower
+        {
+            get { return GetExtent(0); }
+            set { SetExtent(0, value);  }
+        }
+
+        [JsonIgnore]
+        public decimal Upper
+        {
+            get { return GetExtent(1); }
+            set { SetExtent(1, value); }
+        }
+
+        private decimal GetExtent(int index)
+        {
+            if ((Extent?.Count ?? 0) > index)
+                return Extent[index];
+            return 0;
+        }
+        private void SetExtent(int index, decimal value)
+        {
+            if (Extent == null)
+                Extent = new List<decimal>() { 0, 0 };
+            Extent[index] = value;
+        }
+
     }
 
 }
