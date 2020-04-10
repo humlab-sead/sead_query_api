5c426cc65674ad7cd3c67bc1d479493ff1aad184
Roger Mähler <roger.mahler@hotmail.com>
2020-04-10 13:52:24 +0200

    Moved file away from core model

--- sead.query.core/Model/FacetsGraph/GraphRoute.cs
+++ sead.query.core/Model/FacetsGraph/GraphRoute.cs
@@ -15,9 +15,9 @@ namespace SeadQueryCore
         {
         }
 
-        public GraphRoute(List<TableRelation> items)
+        public GraphRoute(IEnumerable<TableRelation> items)
         {
-            Items = items;
+            Items = items.ToList();
         }
 
         public bool Contains(TableRelation item)
@@ -41,33 +41,6 @@ namespace SeadQueryCore
                 return Items.Select(z => z.TargetName).Prepend(Items[0].SourceName).ToList();
             }
             return new List<string>();
-        }
-
-    }
-    public static class GraphRouteUtility
-    {
-        public static bool ExistsAny(List<GraphRoute> routes, TableRelation item)
-        {
-            return routes.Any(x => x.Contains(item));
-        }
-
-        public static List<GraphRoute> Reduce(List<GraphRoute> routes)
-        {
-            List<GraphRoute> reduced_routes = new List<GraphRoute>();
-            foreach (var route in routes)
-            {
-                GraphRoute reduced_route = route.ReduceBy(reduced_routes);
-                if (reduced_route.Items.Count > 0)
-                {
-                    reduced_routes.Add(reduced_route);
-                }
-            }
-            return reduced_routes;
-        }
-
-        public static string ToString(List<GraphRoute> routes)
-        {
-            return String.Join("\n", routes.Select(z => $"{routes.IndexOf(z)};{z.ToString()}"));
         }
     }
 }
