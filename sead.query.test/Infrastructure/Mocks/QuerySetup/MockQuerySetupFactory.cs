722bac9b474af7340ca64b5f0e2c69a91a57cd76
Roger Mähler <roger.mahler@hotmail.com>
2020-04-10 14:13:53 +0200

    Test class rename + added service locator

---
+++ sead.query.test/Infrastructure/Mocks/QuerySetup/MockQuerySetupFactory.cs
@@ -0,0 +1,66 @@
+﻿using SeadQueryCore;
+using System.Collections.Generic;
+using System.Linq;
+using SeadQueryInfra;
+using SeadQueryCore.QueryBuilder;
+using Autofac.Features.Indexed;
+using SeadQueryTest.Fixtures;
+using SeadQueryTest;
+using Xunit;
+using System;
+using SeadQueryTest.Infrastructure;
+using SeadQueryTest.Mocks;
+using Moq;
+
+namespace SeadQueryTest.Fixtures
+{
+    public class MockQuerySetupFactory
+    {
+        public IRepositoryRegistry Registry { get; set; }
+
+        public MockQuerySetupFactory(IRepositoryRegistry registry)
+        {
+            Registry = registry;
+        }
+
+        private Facet GetFacet(string facetCode) => !string.IsNullOrEmpty(facetCode) ? Registry.Facets.GetByCode(facetCode) : null;
+        // FacetInstances.Store[facetCode]
+
+        private Mock<PickFilterCompilerLocator> ConcretePickCompilers()
+        {
+            var mock = new Mock<PickFilterCompilerLocator>();
+            mock.Setup(x => x.Locate(It.Is<EFacetType>(x => x == EFacetType.Discrete)))
+                .Returns(new DiscreteFacetPickFilterCompiler());
+            mock.Setup(x => x.Locate(It.Is<EFacetType>(x => x == EFacetType.Range)))
+                .Returns(new RangeFacetPickFilterCompiler());
+            return mock;
+        }
+
+        public QuerySetup Scaffold(string uri)
+        {
+            var facetsConfigScaffolder = new MockFacetsConfigFactory(Registry.Facets);
+            var facetsConfig = facetsConfigScaffolder.Create(uri);
+            var edgeCompiler = new EdgeSqlCompiler();
+            var pickCompilers = ConcretePickCompilers();
+            var facetsGraph = ScaffoldUtility.DefaultFacetsGraph(Registry);
+            var facetCodes = facetsConfig.GetFacetCodes().AddIfMissing(facetsConfig.TargetFacet.FacetCode);
+            var extraTables = new List<string>();
+
+            QuerySetupCompiler compiler = new QuerySetupCompiler(facetsGraph, pickCompilers.Object, edgeCompiler);
+
+            var querySetup = compiler.Build(
+                facetsConfig,
+                facetsConfig.TargetFacet,
+                extraTables,
+                facetCodes
+            );
+            return querySetup;
+        }
+
+        //public QuerySetup Build(FacetsConfig2 facetsConfig, Facet facet, List<string> extraTables = null, IEnumerable<FacetTable> aliases = null)
+        //{
+        //    List<string> facetCodes = facetsConfig.GetFacetCodes().AddIfMissing(facet.FacetCode);
+        //    return Build(facetsConfig, facet, extraTables ?? new List<string>(), facetCodes, aliases);
+        //}
+    }
+}
