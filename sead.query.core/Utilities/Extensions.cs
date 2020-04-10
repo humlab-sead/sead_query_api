88408f6e8687d45e2c75472738273dd995b5f05e
Roger Mähler <roger.mahler@hotmail.com>
2020-04-10 13:53:46 +0200

    Added PairWise IEnumerable extension

--- sead.query.core/Utilities/Extensions.cs
+++ sead.query.core/Utilities/Extensions.cs
@@ -4,12 +4,20 @@ using System.Linq;
 
 namespace SeadQueryCore
 {
-    public static class ForEachExt {
+    public static class IEnumerableExtensions
+    {
         public static void ForEach<T>(this IEnumerable<T> enumeration, Action<T> action)
         {
             foreach (T item in enumeration) {
                 action(item);
             }
+        }
+
+        public static IEnumerable<TResult> PairWise<TSource, TResult>(
+            this IEnumerable<TSource> source,
+            Func<TSource, TSource, TResult> selector)
+        {
+            return Enumerable.Zip(source, source.Skip(1), selector);
         }
     }
 }
