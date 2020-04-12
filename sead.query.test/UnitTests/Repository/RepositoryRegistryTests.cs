445d5b59f164ba10a764c617021bfe4ab148208f
Roger Mähler <roger.mahler@hotmail.com>
2020-04-12 14:36:10 +0200

    Removed deprecated test

--- sead.query.test/UnitTests/Repository/RepositoryRegistryTests.cs
+++ sead.query.test/UnitTests/Repository/RepositoryRegistryTests.cs
@@ -3,33 +3,34 @@ using SeadQueryCore;
 using SeadQueryInfra;
 using SeadQueryTest.Infrastructure;
 using SeadQueryTest.Mocks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
 using Xunit;
 
 namespace SeadQueryTest.Repository
 {
+    [Collection("JsonSeededFacetContext")]
     public class RepositoryRegistryTests : DisposableFacetContextContainer
-    {
+        {
         public RepositoryRegistryTests(JsonSeededFacetContextFixture fixture) : base(fixture)
         {
         }
 
-        private RepositoryRegistry CreateRepositoryRegistry()
-        {
-            return new RepositoryRegistry(Context);
-        }
+        //[Fact]
+        //public void GetRepositoryTypes_LoadsTypes()
+        //{
+        //    // Arrange
+        //    var registry = new RepositoryRegistry(FacetContext);
+        //    // Act
+        //    var result = registry.GetRepositoryTypes().ToList();
 
-        [Fact(Skip="Not implemented")]
-        public void Commit_StateUnderTest_ExpectedBehavior()
-        {
-            // Arrange
-            var repositoryRegistry = this.CreateRepositoryRegistry();
+        //    // Assert
+        //    Assert.NotNull(result);
+        //    Assert.True(result.Any());
+        //}
 
-            // Act
-            var result = repositoryRegistry.Commit();
-
-            // Assert
-            Assert.True(false);
-        }
 
     }
 }
