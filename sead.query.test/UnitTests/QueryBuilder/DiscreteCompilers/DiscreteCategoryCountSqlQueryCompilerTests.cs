ac39cd090afa7fa2ab31e41c78f65d93dede4a38
Roger Mähler <roger.mahler@hotmail.com>
2020-04-13 11:28:54 +0200

    Fixed test

--- sead.query.test/UnitTests/QueryBuilder/DiscreteCompilers/DiscreteCategoryCountSqlQueryCompilerTests.cs
+++ sead.query.test/UnitTests/QueryBuilder/DiscreteCompilers/DiscreteCategoryCountSqlQueryCompilerTests.cs
@@ -1,53 +1,46 @@
 using Moq;
 using SeadQueryCore;
 using SeadQueryCore.QueryBuilder;
+using SeadQueryTest.Fixtures;
+using SeadQueryTest.Infrastructure;
 using System;
+using System.Text.RegularExpressions;
 using Xunit;
 
 namespace SeadQueryTest.QueryBuilder.DiscreteCompilers
 {
-    public class DiscreteCategoryCountSqlQueryCompilerTests : IDisposable
+    [Collection("JsonSeededFacetContext")]
+    public class DiscreteCategoryCountSqlQueryCompilerTests : DisposableFacetContextContainer
     {
-        private MockRepository mockRepository;
-
-
-
-        public DiscreteCategoryCountSqlQueryCompilerTests()
+        public DiscreteCategoryCountSqlQueryCompilerTests(JsonSeededFacetContextFixture fixture) : base(fixture)
         {
-            this.mockRepository = new MockRepository(MockBehavior.Strict);
-
-
         }
 
-        public void Dispose()
-        {
-            this.mockRepository.VerifyAll();
-        }
-
-        private DiscreteCategoryCountSqlQueryCompiler CreateDiscreteCategoryCountSqlQueryCompiler()
-        {
-            return new DiscreteCategoryCountSqlQueryCompiler();
-        }
-
-        [Fact(Skip = "Not implemented")]
-        public void Compile_StateUnderTest_ExpectedBehavior()
+        [Theory]
+        [InlineData("sites:sites", "sites", "count")]
+        public void Compile_StateUnderTest_ExpectedBehavior(string uri, string facetCode, string aggType)
         {
             // Arrange
-            var discreteCategoryCountSqlQueryCompiler = this.CreateDiscreteCategoryCountSqlQueryCompiler();
-            QuerySetup query = null;
-            Facet facet = null;
-            Facet countFacet = null;
-            string aggType = null;
+            var querySetupMockFactory = new MockQuerySetupFactory(Registry);
+            var querySetup = querySetupMockFactory.Scaffold(uri);
+            var facet = Registry.Facets.GetByCode(facetCode);
+            var countFacet = facet;
 
             // Act
-            var result = discreteCategoryCountSqlQueryCompiler.Compile(
-                query,
-                facet,
-                countFacet,
-                aggType);
+
+            var discreteCategoryCountSqlQueryCompiler = new DiscreteCategoryCountSqlQueryCompiler();
+            var result = discreteCategoryCountSqlQueryCompiler.Compile(querySetup, facet, countFacet, aggType);
+
+            result = Regex.Replace(result, @"\s+", " ").Trim();
 
             // Assert
-            Assert.True(false);
+            //@"select\s+(.*?)\s*from\s+(.*?)\s*(where\s(.*?)\s*)?;"
+            var outerExpected = $@"SELECT category, {aggType}(value) AS count FROM \(.*\) AS x GROUP BY category;";
+            Assert.Matches(outerExpected, result);
+
+            var expected = $@"\s*SELECT category, {aggType}\(value\).*AS count\s+FROM.*\(\s*SELECT.*AS category,.*AS value\s*FROM\s*.*"; // WHERE.*"; // GROUP BY.+\) AS x\s*GROUP BY category;\s*";
+            Assert.Matches(expected, result);
+
         }
     }
 }
