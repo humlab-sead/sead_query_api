8a26d78de2c7cdadf8fb553508a9d92bb64967e9
Roger Mähler <roger.mahler@hotmail.com>
2020-04-14 20:24:29 +0200

    Fixed indentation

--- sead.query.core/QueryBuilder/ResultCompilers/ResultQuerySetup.cs
+++ sead.query.core/QueryBuilder/ResultCompilers/ResultQuerySetup.cs
@@ -6,8 +6,12 @@ namespace SeadQueryCore.QueryBuilder
     public class ResultQuerySetup
     {
         public List<ResultAggregateField> Fields { get; set; }
-        public List<ResultField> ResultFields => Fields.Select(z => z.ResultField).ToList();
-        public List<string> DataTables => Fields.Select(z => z.ResultField.TableName).Where(t => t != null).ToList();
+
+        public List<ResultField> ResultFields =>
+            Fields.Select(z => z.ResultField).ToList();
+
+        public List<string> TableNames =>
+            Fields.Select(z => z.ResultField.TableName).Where(t => t != null).ToList();
 
         public List<(string, string)> AliasPairs { get; set; }
         public List<string> DataFields { get; set; }
@@ -17,13 +21,24 @@ namespace SeadQueryCore.QueryBuilder
 
         public ResultQuerySetup(List<ResultAggregateField> fields)
         {
-            var aliasedFields = fields.Select((field, i) => new { Field = field, Alias = "alias_" + (i+1).ToString() });
+            var aliasedFields =
+                fields.Select((field, i) =>
+                    new {
+                        Field = field,
+                        Alias = "alias_" + (i+1).ToString()
+                    }
+                );
+
             Fields = fields;
             InnerGroupByFields = aliasedFields.Select(p => p.Alias).ToList();
             GroupByFields = aliasedFields.Where(z => z.Field.FieldType.IsGroupByField).Select(z => z.Alias).ToList();
             AliasPairs = aliasedFields.Select(z => ((z.Field.ResultField.ColumnName, z.Alias))).ToList();
             SortFields = aliasedFields.Where(z => z.Field.FieldType.IsSortField).Select(z => z.Alias).ToList();
-            DataFields = aliasedFields.Where(z => z.Field.FieldType.IsResultValue).Select(z => z.Field.FieldType.Compiler.Compile(z.Alias)).ToList();
+
+            DataFields = aliasedFields
+                .Where(z => z.Field.FieldType.IsResultValue)
+                .Select(z => z.Field.FieldType.Compiler.Compile(z.Alias))
+                .ToList();
         }
 
         public bool IsEmpty => (Fields?.Count ?? 0) == 0;
