36778917ef8205d1acf88901058fdbfc83bed583
Roger Mähler <roger.mahler@hotmail.com>
2020-04-14 19:09:36 +0200

    Added FindByName method

--- sead.query.infra/Repository/TableRepository.cs
+++ sead.query.infra/Repository/TableRepository.cs
@@ -22,6 +22,15 @@ namespace SeadQueryInfra
         {
             return Context.Set<TableRelation>().BuildEntity().ToList();
         }
+
+        public TableRelation FindByName(string sourceName, string targetName)
+        {
+            return Context.Set<TableRelation>().BuildEntity()
+                .Where(r => (r.SourceName == sourceName && r.TargetName == targetName) ||
+                            (r.SourceName == targetName && r.TargetName == sourceName))
+                    .FirstOrDefault();
+        }
+
     }
 
     public static class EdgeRepositoryEagerBuilder {
