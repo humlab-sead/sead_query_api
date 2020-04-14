194ee6afba7dc170bcdc7cda4784c737322c9bde
Roger Mähler <roger.mahler@hotmail.com>
2020-04-14 12:59:34 +0200

    Implemented test

--- sead.query.test/UnitTests/QueryBuilder/DiscreteCompilers/DiscreteCategoryCountSqlQueryCompilerTests.cs
+++ sead.query.test/UnitTests/QueryBuilder/DiscreteCompilers/DiscreteCategoryCountSqlQueryCompilerTests.cs
@@ -17,8 +17,10 @@ namespace SeadQueryTest.QueryBuilder.DiscreteCompilers
         }
 
         [Theory]
-        [InlineData("sites:sites", "sites", "count")]
-        public void Compile_StateUnderTest_ExpectedBehavior(string uri, string facetCode, string aggType)
+        [InlineData("sites:sites", "sites", "count", 0)]
+        [InlineData("country:sites", "sites", "count", 1)]
+        [InlineData("country@57:sites@3", "sites", "count", 1)]
+        public void Compile_StateUnderTest_ExpectedBehavior(string uri, string facetCode, string aggType, int expectedJoinCount)
         {
             // Arrange
             var querySetupMockFactory = new MockQuerySetupFactory(Registry);
@@ -31,16 +33,34 @@ namespace SeadQueryTest.QueryBuilder.DiscreteCompilers
             var discreteCategoryCountSqlQueryCompiler = new DiscreteCategoryCountSqlQueryCompiler();
             var result = discreteCategoryCountSqlQueryCompiler.Compile(querySetup, facet, countFacet, aggType);
 
-            result = Regex.Replace(result, @"\s+", " ").Trim();
+            result = result.Squeeze();
 
             // Assert
-            //@"select\s+(.*?)\s*from\s+(.*?)\s*(where\s(.*?)\s*)?;"
-            var outerExpected = $@"SELECT category, {aggType}(value) AS count FROM \(.*\) AS x GROUP BY category;";
+
+            var outerExpected = $@"SELECT category, {aggType}\(value\) AS count FROM \((?<innerSql>.*)\) AS x GROUP BY category;";
             Assert.Matches(outerExpected, result);
 
-            var expected = $@"\s*SELECT category, {aggType}\(value\).*AS count\s+FROM.*\(\s*SELECT.*AS category,.*AS value\s*FROM\s*.*"; // WHERE.*"; // GROUP BY.+\) AS x\s*GROUP BY category;\s*";
-            Assert.Matches(expected, result);
+            var rx = Regex.Match(result, outerExpected);
 
-        }
+            var innerSql = rx.Groups["innerSql"].Value.Squeeze();
+            var innerExpected = @"SELECT (?<tableName>\w+).(?<catName>\w+) AS category, (?:[\w\.]+).(?:[\w\.]+) AS value FROM (?:[\w\.]+) (?:AS \w*)?(?<joins>.*)?\s?WHERE 1 = 1 (?<criterias>.*)?\s?GROUP BY \1.\2, \1.\2";
+            Assert.Matches(innerExpected, innerSql);
+
+            rx = Regex.Match(result, innerExpected);
+
+            var joinSql = (rx.Groups["joins"]?.Value ?? "").Squeeze();
+            if (!joinSql.IsEmpty()) {
+                var joinExpected = @"(?<joins>(?:INNER|LEFT|RIGHT|OUTER)?\s?JOIN\s[\w\.""]+\sON\s[\w\.""]+\s=\s[\w\.""]+\s?)+";
+                Assert.Matches(joinExpected, joinSql);
+                rx = Regex.Match(joinSql, joinExpected);
+                Assert.Equal(expectedJoinCount, rx.Groups.Count);
+            }
+
+            var criteriaSql = (rx.Groups["criterias"]?.Value ?? "").Squeeze();
+            if (!criteriaSql.IsEmpty()) {
+                var criteriaExpected = ".*AND*.";
+                Assert.Matches(criteriaExpected, criteriaSql);
+            }
+         }
     }
 }
