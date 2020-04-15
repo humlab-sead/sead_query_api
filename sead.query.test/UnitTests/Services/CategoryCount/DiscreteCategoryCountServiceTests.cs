f645c230cc4bfd501b683558f3877da7aeccaf34
Roger Mähler <roger.mahler@hotmail.com>
2020-04-15 18:10:08 +0200

    Fixed test

--- sead.query.test/UnitTests/Services/CategoryCount/DiscreteCategoryCountServiceTests.cs
+++ sead.query.test/UnitTests/Services/CategoryCount/DiscreteCategoryCountServiceTests.cs
@@ -1,13 +1,51 @@
 using Moq;
 using SeadQueryCore;
 using SeadQueryCore.QueryBuilder;
+using SeadQueryTest.Infrastructure;
+using SeadQueryTest.Mocks;
 using System;
 using Xunit;
 
 namespace SeadQueryTest.Services.CategoryCount
 {
-    public class DiscreteCategoryCountServiceTests
+
+    [Collection("JsonSeededFacetContext")]
+    public class CategoryCountServiceTests : DisposableFacetContextContainer
     {
-   
+        public CategoryCountServiceTests(JsonSeededFacetContextFixture fixture) : base(fixture)
+        {
+        }
+
+        [Theory]
+        [InlineData("sites:sites")]
+        public void Load_UsingVariousConfigs_LoadsSuccessfully(string uri)
+        {
+            // Arrange
+            var fakeSettings = FakeFacetSetting();
+            var fakeRegistry = FakeRegistry();
+
+            var fakeFacetsConfig = new MockFacetsConfigFactory(Registry.Facets).Create(uri);
+
+            var mockQuerySetupBuilder = MockQuerySetupBuilder(new QuerySetup { /* not used */ });
+            var mockCategoryCountSqlQueryCompiler = MockDiscreteCategoryCountSqlQueryCompiler(returnSql: "SELECT * FROM foot.bar");
+            var queryProxy = new MockTypedQueryProxyFactory()
+                .Create<DiscreteCountDataReaderBuilder, CategoryCountItem>(3);
+
+            // Act
+
+            var service = new DiscreteCategoryCountService(
+                fakeSettings,
+                fakeRegistry,
+                mockQuerySetupBuilder.Object,
+                mockCategoryCountSqlQueryCompiler.Object,
+                queryProxy.Object
+            );
+
+            var result = service.Load(fakeFacetsConfig.TargetCode, fakeFacetsConfig);
+
+            // Assert
+            Assert.NotNull(result);
+        }
+ 
     }
 }
