3721eb1d9654b7e76b50a78377c39cb10d0273c8
Roger Mähler <roger.mahler@hotmail.com>
2020-04-15 18:09:53 +0200

    Fixed test

--- sead.query.test/UnitTests/QueryBuilder/DiscreteCompilers/DiscreteContentSqlQueryBuilderTests.cs
+++ sead.query.test/UnitTests/QueryBuilder/DiscreteCompilers/DiscreteContentSqlQueryBuilderTests.cs
@@ -1,44 +1,35 @@
 using Moq;
 using SeadQueryCore;
 using SeadQueryCore.QueryBuilder;
+using SeadQueryTest.Infrastructure;
+using SeadQueryTest.Mocks;
 using System;
 using Xunit;
 
 namespace SeadQueryTest.QueryBuilder.DiscreteCompilers
 {
-    public class DiscreteContentSqlQueryBuilderTests : IDisposable
+    [Collection("JsonSeededFacetContext")]
+    public class DiscreteContentSqlQueryBuilderTests : DisposableFacetContextContainer
     {
-        private MockRepository mockRepository;
-
-
-
-        public DiscreteContentSqlQueryBuilderTests()
+        public DiscreteContentSqlQueryBuilderTests(JsonSeededFacetContextFixture fixture) : base(fixture)
         {
-            this.mockRepository = new MockRepository(MockBehavior.Strict);
-
-
         }
 
-        public void Dispose()
-        {
-            this.mockRepository.VerifyAll();
-        }
-
-        private DiscreteContentSqlQueryBuilder CreateDiscreteContentSqlQueryBuilder()
-        {
-            return new DiscreteContentSqlQueryBuilder();
-        }
-
-        [Fact(Skip = "Not implemented")]
-        public void Compile_StateUnderTest_ExpectedBehavior()
+        [Theory]
+        [InlineData("sites:sites")]
+        [InlineData("country:country")]
+        [InlineData("ecocode:ecocode")]
+        public void Compile_StateUnderTest_ExpectedBehavior(string uri)
         {
             // Arrange
-            var discreteContentSqlQueryBuilder = this.CreateDiscreteContentSqlQueryBuilder();
+            var facetsConfig = new MockFacetsConfigFactory(Registry.Facets).Create(uri);
             QuerySetup query = null;
             Facet facet = null;
             string text_filter = null;
 
             // Act
+            var discreteContentSqlQueryBuilder = new DiscreteContentSqlCompiler();
+
             var result = discreteContentSqlQueryBuilder.Compile(
                 query,
                 facet,
