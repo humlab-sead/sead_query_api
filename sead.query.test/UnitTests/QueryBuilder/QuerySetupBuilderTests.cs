56a3631902a707cadf4796682428604773b799b8
Roger Mähler <roger.mahler@hotmail.com>
2020-04-19 14:42:57 +0200

    Fixed test

--- sead.query.test/UnitTests/QueryBuilder/QuerySetupBuilderTests.cs
+++ sead.query.test/UnitTests/QueryBuilder/QuerySetupBuilderTests.cs
@@ -1,14 +1,15 @@
 ﻿using Moq;
 using SeadQueryCore;
 using SeadQueryCore.QueryBuilder;
-using SeadQueryTest.Infrastructure;
-using SeadQueryTest.Mocks;
+using SQT.Infrastructure;
+using SQT.Mocks;
 using System;
 using System.Collections.Generic;
+using System.Dynamic;
 using System.Linq;
 using Xunit;
 
-namespace SeadQueryTest.QueryBuilder
+namespace SQT.QueryBuilder
 {
 
     [Collection("JsonSeededFacetContext")]
@@ -23,139 +24,71 @@ namespace SeadQueryTest.QueryBuilder
             return new Dictionary<string, List<GraphRoute>> {
 
                 {
-                    "tbl_denormalized_measured_values_33_0:tbl_denormalized_measured_values_33_0",
+                    "result_facet:sites/result_facet",
                     new List<GraphRoute> {
-                        MockGraphRoute2("tbl_analysis_entities", "tbl_physical_samples", "metainformation.tbl_denormalized_measured_values")
-                    }
-                },
-
-                {
-                    "sites:sites",
-                    new List<GraphRoute> {
-                        MockGraphRoute2("tbl_analysis_entities", "tbl_physical_samples", "tbl_sample_groups", "tbl_sites"),
-                        MockGraphRoute2("tbl_analysis_entities", "tbl_datasets")
+                        FakeRoute2("tbl_analysis_entities", "tbl_physical_samples", "tbl_sample_groups", "tbl_sites"),
+                        FakeRoute2("tbl_analysis_entities", "tbl_datasets")
                     }
                 },
                 {
-                    "country:country",
+                    "result_facet:ecocode/result_facet",
                     new List<GraphRoute> {
-                        MockGraphRoute2("tbl_analysis_entities", "tbl_physical_samples", "tbl_sample_groups", "tbl_sites", "tbl_site_locations"),
-                        MockGraphRoute2("tbl_site_locations", "countries"),
-                        MockGraphRoute2("tbl_analysis_entities", "tbl_datasets")
+                        FakeRoute2("tbl_analysis_entities", "tbl_abundances", "tbl_taxa_tree_master", "tbl_ecocodes", "tbl_ecocode_definitions"),
+                        FakeRoute2("tbl_analysis_entities", "tbl_physical_samples"),
+                        FakeRoute2("tbl_analysis_entities", "tbl_datasets")
                     }
                 },
                 {
-                    "ecocode:sites/ecocode",
+                    "result_facet:tbl_denormalized_measured_values_33_0/result_facet",
                     new List<GraphRoute> {
-                        MockGraphRoute2("tbl_analysis_entities", "tbl_abundances", "tbl_taxa_tree_master", "tbl_ecocodes", "tbl_ecocode_definitions"),
-                        MockGraphRoute2("tbl_analysis_entities", "tbl_physical_samples"),
-                        MockGraphRoute2("tbl_analysis_entities", "tbl_datasets")
+                        FakeRoute2( "tbl_analysis_entities", "facet.method_measured_values(33,0)")
                     }
                 },
-                {
-                    "country:sites/country",
-                    new List<GraphRoute> {
-                        MockGraphRoute2("tbl_analysis_entities", "tbl_physical_samples", "tbl_sample_groups", "tbl_sites", "tbl_site_locations"),
-                        MockGraphRoute2("tbl_site_locations", "countries"),
-                        MockGraphRoute2("tbl_analysis_entities", "tbl_datasets")
-                    }
-                },
-                {
-                    "ecocode:country/sites/ecocode",
-                    new List<GraphRoute> {
-                        MockGraphRoute2("tbl_analysis_entities", "tbl_abundances", "tbl_taxa_tree_master", "tbl_ecocodes", "tbl_ecocode_definitions"),
-                        MockGraphRoute2("tbl_analysis_entities", "tbl_physical_samples"),
-                        MockGraphRoute2("tbl_analysis_entities", "tbl_datasets")
-                    }
-                },
-                {
-                    "sites:country/sites/ecocode",
-                    new List<GraphRoute> {
-                        MockGraphRoute2("tbl_analysis_entities", "tbl_physical_samples", "tbl_sample_groups", "tbl_sites"),
-                        MockGraphRoute2("tbl_analysis_entities", "tbl_datasets")
-                    }
-                }
             };
         }
 
-        //private List<string> GetTargetTables(FacetsConfig2 facetsConfig, Facet computeFacet)
-        //{
-        //    List<string> tables = facetsConfig.TargetFacet.Tables.Select(x => x.ResolvedAliasOrTableOrUdfName).ToList();
-
-        //    if (computeFacet.FacetCode != facetsConfig.TargetFacet.FacetCode)
-        //        tables.AddRange(computeFacet.Tables.Select(x => x.ResolvedAliasOrTableOrUdfName).ToList());
-
-        //    tables = tables.Distinct().ToList();
-        //    return tables;
-        //}
-
-        //private List<string> GetDiscreteTables(FacetsConfig2 facetsConfig, Facet countFacet, Facet targetFacet)
-        //{
-        //    List<string> tables = targetFacet
-        //        .Tables
-        //        .Select(x => x.ResolvedAliasOrTableOrUdfName)
-        //        .ToList();
-
-        //    if (countFacet.FacetCode != targetFacet.FacetCode)
-        //        tables.Add(countFacet.TargetTable.ResolvedAliasOrTableOrUdfName);
-
-        //    return tables.Distinct().ToList();
-        //}
+        [Fact]
+        public void SkitTest ()
+        {
+            string[] trail = { "tbl_analysis_entities", "tbl_abundances", "tbl_taxa_tree_master", "tbl_ecocodes", "tbl_ecocode_definitions" };
+            var pairs = RouteHelper.ToPairs(trail);
+            var splits = pairs.Select(x => x.Split("/")).Select(z => new { Source = z[0], Target = z[1] }).ToList();
+            var relations = splits.Select(z => Registry.TableRelations.FindByName(z.Source, z.Target) ??
+                                 (TableRelation)Registry.TableRelations.FindByName(z.Target, z.Source).Reverse()).ToList();
+            Assert.True(true);
+        }
 
         [Theory]
-        [InlineData("sites:sites")]
-        [InlineData("country:country")]
-        [InlineData("ecocode:ecocode")]
-        public void Build_WhenFacetsConfigIsSingleDiscreteWithoutPicks_ReturnsValidQuerySetup(string uri)
+        [InlineData("result_facet:sites@1/result_facet", "result_facet:sites/result_facet")]
+        [InlineData("result_facet:tbl_denormalized_measured_values_33_0/result_facet", null)]
+        [InlineData("result_facet:ecocode/result_facet", null)]
+        public void Build_WithVariousConfigs_Success(string uri, string routeKey)
         {
             // Arrange
             var facetsConfig = new MockFacetsConfigFactory(Registry.Facets).Create(uri);
             var mockPickCompilers = MockPickCompilerLocator("SELECT * FROM fot.bar");
             var mockJoinCompiler = MockJoinSqlCompiler("A JOIN B ON A.X = B.Y");
-            var mockRoutes = FakeFacetQueryResultRoutes()[uri];
+            var mockRoutes = FakeFacetQueryResultRoutes()[routeKey ?? uri];
             var mockFacetsGraph = MockFacetsGraph(mockRoutes);
             var facetCodes = new List<string>() { facetsConfig.TargetCode };
             var extraTables = new List<string>();
 
-            // FIXME: We must add a result facet!!!
-
-            //Facet facet = facetRepository.GetByCode(facetCode);
-            //Facet countFacet = facetRepository.Get(facet.AggregateFacetId); // default to ID 1 = "result_facet"
-
-            //string targetCode = Utility.Coalesce(facetsConfig?.TargetCode, countFacet.FacetCode);
-
-            //Facet targetFacet = facetRepository.GetByCode(targetCode);
-            //List<string> tables = GetDiscreteTables(facetsConfig, countFacet, targetFacet);
-
-            //List<string> facetCodes = facetsConfig.GetFacetCodes();
-            //facetCodes.InsertAt(targetFacet.FacetCode, countFacet.FacetCode);
-
             // Act
-            var compiler = new QuerySetupBuilder(
-                mockFacetsGraph.Object,
-                mockPickCompilers.Object,
-                mockJoinCompiler.Object
-            );
+            var compiler = new QuerySetupBuilder(mockFacetsGraph.Object, mockPickCompilers.Object, mockJoinCompiler.Object);
 
             QuerySetup querySetup = compiler.Build(facetsConfig, facetsConfig.TargetFacet, extraTables, facetCodes);
-            // ScaffoldUtility.Dump(querySetup, "");
+
+            //DumpUriObject(uri, querySetup);
 
             // Assert
 
-            Assert.NotNull(querySetup);
-
-            // FIXME Some relevant assertions!
-            
-            //var expected = expectedRoutes;
-            //Assert.NotNull(querySetup);
-            //Assert.Equal(countFacet.FacetCode, querySetup.Facet.FacetCode);
-            //for (var i = 0; i < expected.Count; i++) {
-            //    Assert.Equal(expected[i].Count, querySetup.Routes[i].Items.Count);
-            //    for (var j = 0; j < expected[i].Count; j++) {
-            //        Assert.Equal(expected[i][j], querySetup.Routes[i].Items[j].ToStringPair());
-            //    }
-            //}
-            // for (var route in querySetup.reduced_route) TestContext.WriteLine(querySetup.reduced_routes.IndexOf(route) + ": " + route.ToString());
+            Assert.Same(facetsConfig.TargetConfig, querySetup.TargetConfig);
+            Assert.Same(facetsConfig.TargetFacet, querySetup.Facet);
+            Assert.Equal(facetsConfig.FacetConfigs.Where(z => z.HasConstraints()).Count(), querySetup.Criterias.Count);
+            Assert.True(AreEqualByProperty(mockRoutes, querySetup.Routes));
+            Assert.Equal(mockRoutes.Aggregate(0, (i,z) => i + z.Items.Count), querySetup.Joins.Count);
         }
+
+
     }
 }
