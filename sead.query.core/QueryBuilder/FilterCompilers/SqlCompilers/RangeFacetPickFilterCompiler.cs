835c805753288948c7c16e316226fd80c560053a
Roger Mähler <roger.mahler@hotmail.com>
2020-04-21 19:27:04 +0200

    Code cleanup

--- sead.query.core/QueryBuilder/FilterCompilers/SqlCompilers/RangeFacetPickFilterCompiler.cs
+++ sead.query.core/QueryBuilder/FilterCompilers/SqlCompilers/RangeFacetPickFilterCompiler.cs
@@ -6,11 +6,14 @@ namespace SeadQueryCore.QueryBuilder
 
         public string Compile(Facet targetFacet, Facet currentFacet, FacetConfig2 config)
         {
-            var picks = config.GetPickValues(true);
-            return picks.Count != 2 ? ""
-                : SqlCompileUtility.BetweenExpr(currentFacet.CategoryIdExpr, picks[0], picks[1])
-                    .GlueIf(currentFacet.QueryCriteria, " AND ");
-        }
+            if (!config.HasPicks())
+                return currentFacet.QueryCriteria;
 
+            var picks = config.GetPickValues(true);
+
+            return SqlCompileUtility.BetweenExpr(currentFacet.CategoryIdExpr, picks[0], picks[1])
+                .GlueIf(currentFacet.QueryCriteria, " AND ");
+
+        }
     }
 }
