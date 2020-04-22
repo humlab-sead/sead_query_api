96f1823f34748495071d55666de5f7480fa60756
Roger Mähler <roger.mahler@hotmail.com>
2020-04-22 18:23:44 +0200

    Fixture updated

--- sead.query.test/Infrastructure/Data/ScaffoldJsonFixtures.cs
+++ sead.query.test/Infrastructure/Data/ScaffoldJsonFixtures.cs
@@ -35,45 +35,23 @@ namespace SQT.Infrastructure.Scaffolding
             return new FacetContext(builder.Options);
         }
 
-        //[Fact(Skip = "Not a test. Scaffolds JSON data from")]
-        //public void ScaffoldFacetConfigsToJsonFile()
-        //{
-        //    var serializer = CreateSerializer();
-        //    var path = ScaffoldUtility.JsonDataFolder();
-        //    using (var fixture = new JsonSeededFacetContextFixture())
-        //    using (var container = new DisposableFacetContextContainer(fixture)) {
-        //        var context = container.Context;
-
-        //        new JsonWriterService(serializer).SerializeTypesToPath(container.Context, ScaffoldUtility.GetModelTypes(), path);
-        //    }
-        //}
-
-        [Fact(Skip = "Not a test. Scaffolds JSON data from online facet model")]
-        public void ScaffoldFacetDatabaseModelToJsonFileUsingOnlineDatabase()
+        /// <summary>
+        /// Serializes a new JSON facet model fixture to from data in online database
+        /// </summary>
+        /// <param name="updateTheModel">Set to true if you want to update fixture</param>
+        [Theory]
+        [InlineData(false)]
+        public void UpdateFacetContextFixture_IfParameterIsSetToTrue(bool updateTheModel)
         {
+            if (!updateTheModel)
+                return;
+
             var serializer = CreateSerializer();
             var path = ScaffoldUtility.JsonDataFolder();
+
             using (var context = CreateContext()) {
                 new JsonWriterService(serializer).SerializeTypesToPath(context, ScaffoldUtility.GetModelTypes(), path);
             }
         }
-
-        [Fact(Skip = "Not a test.")]
-        public void CanLoadScaffoldedJsonModelAsDbContext()
-        {
-            JsonSerializer serializer = new JsonSerializer
-            {
-                // serializer.Converters.Add(new JavaScriptDateTimeConverter());
-                NullValueHandling = NullValueHandling.Ignore
-            };
-
-            var reader = new JsonReaderService(DefaultResolver());
-            var path = ScaffoldUtility.JsonDataFolder();
-            using (var context = CreateContext()) {
-                var types = ScaffoldUtility.GetModelTypes();
-                var facets = reader.Deserialize<SeadQueryCore.Facet>(path);
-            }
-        }
-
     }
 }
