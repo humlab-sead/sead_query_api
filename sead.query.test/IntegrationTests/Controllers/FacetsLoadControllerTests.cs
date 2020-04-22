fce05e20d6979f0c6cb5dcd8f47d2114bb0bdfb6
Roger Mähler <roger.mahler@hotmail.com>
2020-04-22 18:24:57 +0200

    Removed dead code

--- sead.query.test/IntegrationTests/Controllers/FacetsLoadControllerTests.cs
+++ sead.query.test/IntegrationTests/Controllers/FacetsLoadControllerTests.cs
@@ -50,10 +50,12 @@ namespace IntegrationTests
                 Assert.NotEmpty(await response.Content.ReadAsStringAsync());
             }
         }
+
         [Theory]
-        //[InlineData("sites:sites")]
+        [InlineData("relative_age_name:relative_age_name")]
+        [InlineData("master_dataset:master_dataset@1")]
         [InlineData("sites:country@5/sites")]
-        public async Task Load_DiscreteFacetConfigs_IsLoaded(string uri)
+        public async Task Load_VariousFacetConfigs_IsLoaded(string uri)
         {
             var facetsConfig = MockService.FakeFacetsConfig(uri);
             var json = JsonConvert.SerializeObject(facetsConfig);
@@ -79,43 +81,6 @@ namespace IntegrationTests
 
             }
         }
-
-        //private FacetsController CreateFacetsController()
-        //{
-        //    var mockLoadFacetService = new Mock<IFacetReconstituteService>();
-        //    var mockReconstituteService = new Mock<IFacetConfigReconstituteService>();
-
-        //    return new FacetsController(
-        //        Registry,
-        //        mockReconstituteService.Object,
-        //        mockLoadFacetService.Object
-        //    );
-
-        //}
-
-        //[Fact(Skip = "Not working")]
-        //public async Task CanLoadDiscreteFacetConfigsWithPicks()
-        //{
-        //    var data = new FacetConfigsByUriFixtures();
-        //    var builder = new SeadTestHostBuilder().Create<TestStartup<TestDependencyService>>();
-        //    using (var host = await builder.StartAsync()) {
-        //        using (var client = host.GetTestClient()) {
-        //            foreach (var item in data.DiscreteTestConfigsWithPicks) {
-        //                var uri = item[0].ToString();
-        //                var expectedCount = item[2];
-        //                FacetsConfig2 facetsConfig = facetsConfigMockFactory.Create(uri);
-
-        //                var json = JsonConvert.SerializeObject(facetsConfig);
-        //                var request_content = new StringContent(json, Encoding.UTF8, "application/json");
-        //                var response = await client.PostAsync(new Uri("/api/facets/load"), request_content);
-        //                response.EnsureSuccessStatusCode();
-        //                var response_content = await response.Content.ReadAsStringAsync();
-        //                var facetContent = JsonConvert.DeserializeObject<FacetContent>(response_content);
-        //                Assert.Equal(expectedCount, facetContent.Items.Count);
-        //            }
-        //        }
-        //    }
-        //}
 
     }
 }
