cdf9176cb56f1bf6fa3a87692ce1c44417d1c6ce
Roger Mähler <roger.mahler@hotmail.com>
2020-04-30 14:29:29 +0200

    Draft test

--- sead.query.test/UnitTests/Services/DiscreteBogusPickServiceTests.cs
+++ sead.query.test/UnitTests/Services/DiscreteBogusPickServiceTests.cs
@@ -1,30 +1,46 @@
 using Moq;
 using SeadQueryCore;
 using SeadQueryCore.QueryBuilder;
+using SQT.Infrastructure;
 using System;
+using System.Collections.Generic;
 using Xunit;
 
 namespace SQT.Services
 {
-    public class DeleteBogusPickServiceTests : IDisposable
+    public class DeleteBogusPickServiceTests : DisposableFacetContextContainer
     {
-        [Fact(Skip = "Not implemented")]
-        public void Delete_StateUnderTest_ExpectedBehavior()
-        {
-            //// Arrange
-            //var service = this.CreateService();
-            //FacetsConfig2 facetsConfig = null;
-
-            //// Act
-            //var result = service.Delete(
-            //    facetsConfig);
-
-            //// Assert
-            //Assert.True(false);
-        }
-
-        public void Dispose()
+        public DeleteBogusPickServiceTests(JsonSeededFacetContextFixture fixture) : base(fixture)
         {
         }
+
+        protected virtual Mock<IValidPicksSqlCompiler> MockValidPickCompiler(string returnSql)
+        {
+            var mockCompiler = new Mock<IValidPicksSqlCompiler>();
+            mockCompiler
+                .Setup(z => z.Compile(It.IsAny<QuerySetup>(), It.IsAny<Facet>(), It.IsAny<List<int>>()))
+                .Returns(returnSql);
+            return mockCompiler;
+        }
+
+        //[Theory]
+        //[InlineData("sites:sites")]
+        //public void Delete_StateUnderTest_ExpectedBehavior(string uri)
+        //{
+        //    //// Arrange
+        //    var fakeQuerySetup = FakeQuerySetup(uri);
+        //    var mockValidPicksCompiler = MockValidPickCompiler("#SQL-QUERY#");
+        //    List<CategoryCountItem> fakeValues = null;
+        //    var mockQueryProxy = MockTypedQueryProxy(fakeValues);
+
+        //    var service = new BogusPickService(FakeRegistry(), fakeQuerySetup, mockValidPicksCompiler.Object, mockQueryProxy.Object);
+        //    //// Act
+        //    //var result = service.Delete(
+        //    //    facetsConfig);
+
+        //    //// Assert
+        //    //Assert.True(false);
+        //}
+
     }
 }
