4e0934ca63446e464f07b69402221a0523177f44
Roger Mähler <roger.mahler@hotmail.com>
2020-05-02 18:20:48 +0200

    Removed dead code

--- sead.query.test/Infrastructure/Mocks/FacetContext/JsonSeededFacetContextFactory.cs
+++ sead.query.test/Infrastructure/Mocks/FacetContext/JsonSeededFacetContextFactory.cs
@@ -8,7 +8,6 @@ namespace SQT.Mocks
 {
     internal static class JsonSeededFacetContextFactory
     {
-        //public static async Task<FacetContext> Create(DbContextOptions options, string folder)
         public static FacetContext Create(DbContextOptions options, JsonSeededFacetContextFixture fixture)
         {
             using (var context = new JsonSeededFacetContext(options, fixture)) {
@@ -17,24 +16,5 @@ namespace SQT.Mocks
             }
             return new FacetContext(options);
         }
-
-        //public static FacetContext Create(DbContextOptions options)
-        //{
-        //    return Create(options, ScaffoldUtility.JsonDataFolder());
-        //}
-
-        //public static FacetContext Create(DbConnection connection)
-        //{
-        //    var options = SqliteContextOptionsFactory.Create(connection);
-        //    return Create(options, ScaffoldUtility.JsonDataFolder());
-        //}
-
-        //public static FacetContext Create()
-        //{
-        //    // FIXME: Refactor away, use using in test cases instead
-        //    var connection = SqliteConnectionFactory.CreateAndOpen();
-        //    return Create(connection);
-        //}
-
     }
 }
