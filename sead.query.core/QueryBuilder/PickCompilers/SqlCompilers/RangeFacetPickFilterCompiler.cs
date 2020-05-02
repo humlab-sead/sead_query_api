1c0643d4afb2a955b70cd5fcc724de505e205338
Roger Mähler <roger.mahler@hotmail.com>
2020-05-02 10:17:16 +0200

    Renamed folder

--- sead.query.core/QueryBuilder/PickCompilers/SqlCompilers/RangeFacetPickFilterCompiler.cs
+++ sead.query.core/QueryBuilder/PickCompilers/SqlCompilers/RangeFacetPickFilterCompiler.cs
@@ -7,12 +7,12 @@ namespace SeadQueryCore.QueryBuilder
         public string Compile(Facet targetFacet, Facet currentFacet, FacetConfig2 config)
         {
             if (!config.HasPicks())
-                return currentFacet.QueryCriteria;
+                return currentFacet.Criteria;
 
             var picks = config.GetPickValues(true);
 
             return SqlCompileUtility.BetweenExpr(currentFacet.CategoryIdExpr, picks[0], picks[1])
-                .GlueIf(currentFacet.QueryCriteria, " AND ");
+                .GlueIf(currentFacet.Criteria, " AND ");
 
         }
     }
