4c40320c4f84cac0a01d3c83cf9c7567940927df
Roger Mähler <roger.mahler@hotmail.com>
2020-05-02 18:13:29 +0200

    Reduced interface

---
+++ sead.query.core/QueryBuilder/IQuerySetupBuilder.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace SeadQueryCore.QueryBuilder
+{
+    public interface IQuerySetupBuilder
+    {
+        IFacetsGraph FacetsGraph { get; set; }
+
+        QuerySetup Build(FacetsConfig2 facetsConfig, Facet facet, List<string> extraTables, List<string> facetCodes);
+        QuerySetup Build(FacetsConfig2 facetsConfig, Facet facet, IEnumerable<ResultAggregateField> fields);
+    }
+}
