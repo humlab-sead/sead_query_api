4c4f744532d7c2a66bddcff772e5e957577ab228
Roger Mähler <roger.mahler@hotmail.com>
2020-05-04 09:21:26 +0200

    Refactor: moved logic to class

---
+++ sead.query.core/QueryBuilder/PickCompilers/IPicksFilterCompiler.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace SeadQueryCore.QueryBuilder
+{
+    public interface IPicksFilterCompiler
+    {
+        IEnumerable<string> Compile(Facet targetFacet, List<FacetConfig2> involvedConfigs);
+    }
+}
---
+++ sead.query.core/QueryBuilder/PickCompilers/PicksFilterCompiler.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace SeadQueryCore.QueryBuilder
+{
+    public class PicksFilterCompiler : IPicksFilterCompiler
+    {
+        public IPickFilterCompilerLocator PickCompilers { get; set; }
+
+        public PicksFilterCompiler(IPickFilterCompilerLocator pickCompilers)
+        {
+            PickCompilers = pickCompilers;
+        }
+
+        /// <summary>
+        /// Returns where-clauses based on user picks for all involved facets
+        /// </summary>
+        /// <param name="targetFacet"></param>
+        /// <param name="involvedConfigs"></param>
+        /// <returns></returns>
+        public IEnumerable<string> Compile(Facet targetFacet, List<FacetConfig2> involvedConfigs)
+        {
+            var criterias = involvedConfigs
+                .Select(
+                    config => PickCompiler(config).Compile(targetFacet, config.Facet, config)
+                 )
+                .Where(
+                    x => x.IsNotEmpty()
+                );
+            return criterias;
+        }
+
+        protected IPickFilterCompiler PickCompiler(FacetConfig2 c)
+        {
+            return PickCompilers.Locate(c.Facet.FacetTypeId);
+        }
+    }
+}
