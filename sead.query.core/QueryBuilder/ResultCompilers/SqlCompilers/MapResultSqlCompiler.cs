d78d7260b8c5cc0a94a838a8f4d682e564dc0415
Roger Mähler <roger.mahler@hotmail.com>
2020-05-04 09:25:41 +0200

    Added debug guard, minor cleanups

--- sead.query.core/QueryBuilder/ResultCompilers/SqlCompilers/MapResultSqlCompiler.cs
+++ sead.query.core/QueryBuilder/ResultCompilers/SqlCompilers/MapResultSqlCompiler.cs
@@ -1,20 +1,23 @@
 ﻿using System.Collections.Generic;
+using System.Diagnostics;
 
 namespace SeadQueryCore
 {
     public class MapResultSqlCompiler : IResultSqlCompiler {
-        // FIXME Check if facet can ce refactored away
-        public string Compile(QueryBuilder.QuerySetup query, Facet facet, IEnumerable<ResultAggregateField> fields)
+        // FIXME Check if facet can be refactored away
+        public string Compile(QueryBuilder.QuerySetup querySetup, Facet facet, IEnumerable<ResultAggregateField> fields)
         {
+
+            Debug.Assert(querySetup.Facet.FacetCode.Equals(facet.FacetCode), "Refactor check: Refactor away facet");
+
             string sql = $@"
             SELECT DISTINCT {facet.CategoryIdExpr} AS id_column, {facet.CategoryNameExpr} AS name, coalesce(latitude_dd, 0.0) AS latitude_dd, coalesce(longitude_dd, 0) AS longitude_dd
-            FROM {query.Facet.TargetTable.ResolvedSqlJoinName}
-                 {query.Joins.Combine("")}
+            FROM {querySetup.Facet.TargetTable.ResolvedSqlJoinName}
+                 {querySetup.Joins.Combine("")}
             WHERE 1 = 1
-            {"AND ".GlueTo(query.Criterias.Combine(" AND "))}
+            {"AND ".GlueTo(querySetup.Criterias.Combine(" AND "))}
         ";
             return sql;
         }
     }
-
 }
