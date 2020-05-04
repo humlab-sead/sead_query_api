dfd7165f59a24936e5ece9fc01ca12b3e17f7365
Roger Mähler <roger.mahler@hotmail.com>
2020-05-04 09:17:41 +0200

    Removed parameter

--- sead.query.core/QueryBuilder/DiscreteCompilers/SqlCompilers/ValidPicksSqCompiler.cs
+++ sead.query.core/QueryBuilder/DiscreteCompilers/SqlCompilers/ValidPicksSqCompiler.cs
@@ -6,17 +6,17 @@ namespace SeadQueryCore
 
     public class ValidPicksSqCompiler : IValidPicksSqlCompiler
     {
-        public string Compile(QueryBuilder.QuerySetup query, Facet facet, List<int> picks)
+        public string Compile(QueryBuilder.QuerySetup query, List<int> picks)
         {
             if (picks.Count == 0)
                 throw new ArgumentException("No picks specified!");
 
             string picks_clause = picks.Combine(",", x => $"('{x}'::text)");
             string sql = $@"
-            SELECT DISTINCT pick_id, {facet.CategoryNameExpr} AS name
+            SELECT DISTINCT pick_id, {query.Facet.CategoryNameExpr} AS name
             FROM {query.Facet.TargetTable.ResolvedSqlJoinName}
             JOIN (VALUES {picks_clause}) AS x(pick_id)
-              ON x.pick_id = {facet.CategoryIdExpr}::text
+              ON x.pick_id = {query.Facet.CategoryIdExpr}::text
               {query.Joins.Combine("")}
             WHERE 1 = 1
               {" AND ".GlueTo(query.Criterias.Combine(" AND "))}
