d82d9af80b108644425f7f62a19887affea25071
Roger Mähler <roger.mahler@hotmail.com>
2020-05-07 13:59:29 +0200

    Moved files

---
+++ sead.query.core/QueryBuilder/JoinCompilers/IJoinsClauseCompiler.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace SeadQueryCore.QueryBuilder
+{
+    public interface IJoinsClauseCompiler
+    {
+        List<string> Compile(FacetsConfig2 facetsConfig, Facet targetFacet, List<string> involvedTables);
+    }
+}
---
+++ sead.query.core/QueryBuilder/JoinCompilers/JoinsClauseCompiler.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace SeadQueryCore.QueryBuilder
+{
+    public class JoinsClauseCompiler: IJoinsClauseCompiler
+    {
+        public JoinsClauseCompiler(IFacetsGraph graph, IJoinSqlCompiler joinCompiler)
+        {
+            FacetsGraph = graph;
+            JoinCompiler = joinCompiler;
+        }
+
+        public IJoinSqlCompiler JoinCompiler { get; }
+        public IFacetsGraph FacetsGraph { get; set; }
+
+        private FacetTable GetFacetTableByNameOrAlias(FacetsConfig2 facetsConfig, TableRelation edge)
+            => facetsConfig.GetFacetTable(edge.TargetName) ?? FacetsGraph.GetAliasedFacetTable(edge.TargetName);
+
+        public virtual List<string> Compile(FacetsConfig2 facetsConfig, Facet targetFacet, List<string> involvedTables)
+        {
+            var routes = FacetsGraph.Find(targetFacet.TargetTable.ResolvedAliasOrTableOrUdfName, involvedTables, true);
+            var joins = routes.Edges().Select(
+                    edge => JoinCompiler.Compile(edge, GetFacetTableByNameOrAlias(facetsConfig, edge), true)
+                ).ToList();
+            return joins;
+        }
+    }
+}
