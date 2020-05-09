516bc8b46eb7168b7289b45d9f51f53dc9c84725
Roger Mähler <roger.mahler@hotmail.com>
2020-05-09 10:53:47 +0200

    Consoildated extensions

---
+++ sead.query.core/Model/Extensions/ResultViewTypeExt.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+using SeadQueryCore.QueryBuilder;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+namespace SeadQueryCore {
+    public static class ResultViewTypeExt
+    {
+        public static IResultSqlCompiler GetSqlCompiler(this ResultViewType viewType)
+        {
+            return (IResultSqlCompiler)Activator.CreateInstance(
+                Type.GetType($"SeadQueryCore.{viewType.SqlCompiler}"), viewType
+            );
+        }
+    }
+}
