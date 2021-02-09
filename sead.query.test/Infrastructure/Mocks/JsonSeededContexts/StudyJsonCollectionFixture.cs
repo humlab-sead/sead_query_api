5bd715af15d042a099d283b5cc757799c8b69445
Roger Mähler <roger.mahler@hotmail.com>
2021-02-09 19:07:11 +0100

    Added generic test facet system

---
+++ sead.query.test/Infrastructure/Mocks/JsonSeededContexts/StudyJsonCollectionFixture.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Xunit;
+
+namespace SQT.Infrastructure
+{
+
+    public class StudyJsonFacetContextFixture : JsonFacetContextFixture
+    {
+        public StudyJsonFacetContextFixture() :
+            base(Path.Combine(ScaffoldUtility.GetRootFolder(), "Infrastructure", "Data", "StudyDb", "Json"))
+        {
+        }
+    }
+
+    [CollectionDefinition("StudyJsonSeededFacetContext")]
+    public class StudyJsonCollectionFixture : ICollectionFixture<StudyJsonFacetContextFixture>
+    {
+        // This class has no code, and is never created. Its purpose is simply
+        // to be the place to apply [CollectionDefinition] and all the
+        // ICollectionFixture<> interfaces.
+    }
+
+}
