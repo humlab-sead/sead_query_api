eda32c90593f364a31cc7d5fde24efa980fd2117
Roger Mähler <roger.mahler@hotmail.com>
2024-03-22 07:55:31 +0100

    Remove unnecessary using statements in Program.cs

--- sead.query.api/Program.cs
+++ sead.query.api/Program.cs
@@ -1,16 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
 using Autofac.Extensions.DependencyInjection;
-using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Serilog;
-using Serilog.Events;
 
 namespace SeadQueryAPI
 {
