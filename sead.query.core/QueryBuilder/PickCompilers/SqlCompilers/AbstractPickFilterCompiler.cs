f00ca2a3c032ccbcf98fc128fa7dec140037d144
Roger Mähler <roger.mahler@hotmail.com>
2024-03-30 23:19:52 +0100

    Add AbstractPickFilterCompiler class

---
+++ sead.query.core/QueryBuilder/PickCompilers/SqlCompilers/AbstractPickFilterCompiler.cs
@@ -0,0 +1,22 @@
+﻿namespace SeadQueryCore.QueryBuilder;
+
+
+public abstract class AbstractPickFilterCompiler(bool applyFilterWhenNoPicks = false, bool applyFilterOnSelf = false) : IPickFilterCompiler
+{
+    protected bool ApplyFilterOnSelf { get; set; } = applyFilterOnSelf;
+    protected bool ApplyFilterWhenNoPicks { get; set; } = applyFilterWhenNoPicks;
+
+    public virtual string Compile(Facet targetFacet, Facet currentFacet, FacetConfig2 config)
+    {
+        if (!ApplyFilterOnSelf && targetFacet.FacetCode == currentFacet.FacetCode)
+            return "";
+
+        if (!ApplyFilterWhenNoPicks && !config.HasPicks())
+            return "";
+
+        return SqlCompileUtility.InExpr(currentFacet.CategoryIdExpr, config.GetPickValues());
+    }
+
+    protected abstract string CompileExpr(Facet targetFacet, Facet currentFacet, FacetConfig2 config);
+
+}
