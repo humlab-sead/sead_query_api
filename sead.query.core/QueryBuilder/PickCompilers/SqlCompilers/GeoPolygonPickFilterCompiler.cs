4b31c260c255ef99bdf5645a9b8321c4e7d71421
Roger Mähler <roger.mahler@hotmail.com>
2024-03-30 23:19:43 +0100

    Add geopolygon compiler

---
+++ sead.query.core/QueryBuilder/PickCompilers/SqlCompilers/GeoPolygonPickFilterCompiler.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace SeadQueryCore.QueryBuilder;
+
+public class GeoPolygonPickFilterCompiler : IPickFilterCompiler
+{
+    public string Compile(Facet targetFacet, Facet currentFacet, FacetConfig2 config)
+    {
+        if (!config.HasPicks())
+            return currentFacet.Criteria;
+
+        var polygon = config.GetPickValues(false);
+
+        if (polygon.Count % 2 != 0 || polygon.Count < 6)
+            throw new ArgumentException("Invalid polygon sizes");
+
+        if (polygon[0] != polygon[^2] || polygon[1] != polygon[^1])
+            polygon.AddRange([polygon[0], polygon[1]]);
+
+        return SqlCompileUtility.WithinPolygonExpr(currentFacet.CategoryIdExpr, polygon)
+            .GlueIf(currentFacet.Criteria, " AND ");
+
+    }
+
+}
