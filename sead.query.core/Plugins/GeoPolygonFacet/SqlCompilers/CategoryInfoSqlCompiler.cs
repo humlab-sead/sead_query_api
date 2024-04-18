9f14925599787fe7eb320cab41a412aa316c7609
Roger Mähler <roger.mahler@hotmail.com>
2024-04-18 15:06:20 +0200

    Added ToItem method & renamed class.

--- sead.query.core/Plugins/GeoPolygonFacet/SqlCompilers/CategoryInfoSqlCompiler.cs
+++ sead.query.core/Plugins/GeoPolygonFacet/SqlCompilers/CategoryInfoSqlCompiler.cs
@@ -1,19 +1,20 @@
-﻿
+﻿using System.Data;
+
 namespace SeadQueryCore
 {
-    public class DiscreteContentSqlCompiler : IDiscreteContentSqlCompiler
+    public class GeoPolygonCategoryInfoSqlCompiler : IDiscreteCategoryInfoSqlCompiler
     {
         public virtual string Compile(QueryBuilder.QuerySetup query, Facet facet, string categoryNameFilter)
         {
             string sql = $@"
-            SELECT cast({facet.CategoryIdExpr} AS varchar) AS category, {facet.CategoryNameExpr} AS name
+            SELECT cast({facet.CategoryIdExpr} AS varchar) AS category, {facet.CategoryNameExpr} AS name 
             FROM {query.Facet.TargetTable.ResolvedSqlJoinName}
                  {query.Joins.Combine("")}
             WHERE 1 = 1
-              {"AND ".GlueTo(CategoryNameLike(facet, categoryNameFilter)) }
+              {"AND ".GlueTo(CategoryNameLike(facet, categoryNameFilter))}
               {"AND ".GlueTo(query.Criterias.Combine(" AND "))}
             GROUP BY 1, 2
-            {SortBy(facet)}";
+            {SortBy(facet)} ";
             return sql;
         }
 
@@ -23,5 +24,15 @@ namespace SeadQueryCore
         private static string SortBy(Facet facet)
             => facet.SortExpr.IsEmpty() ? "" : $", {facet.SortExpr} ORDER BY {facet.SortExpr}";
 
+        public CategoryItem ToItem(IDataReader dr)
+        {
+            return new CategoryItem()
+            {
+                Category = dr.Category2String(0),
+                Count = 0,
+                Extent = [0],
+                Name = dr.Category2String(1) ?? "",
+            };
+        }
     }
 }
