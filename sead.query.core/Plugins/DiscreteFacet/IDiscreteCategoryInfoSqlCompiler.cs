0a962c2f71ce1fabd3f4135de309456cc4158a79
Roger Mähler <roger.mahler@hotmail.com>
2024-04-18 15:11:49 +0200

    Renamed class & file

---
+++ sead.query.core/Plugins/DiscreteFacet/IDiscreteCategoryInfoSqlCompiler.cs
@@ -0,0 +1,10 @@
+﻿using System.Data;
+using SeadQueryCore.QueryBuilder;
+
+namespace SeadQueryCore
+{
+    public interface IDiscreteCategoryInfoSqlCompiler : ICategoryInfoSqlCompiler
+    {
+        string Compile(QuerySetup query, Facet facet, string text_filter);
+    }
+}
