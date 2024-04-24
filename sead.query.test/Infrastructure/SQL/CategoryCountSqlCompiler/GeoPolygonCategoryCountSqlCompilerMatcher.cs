64d54bbf45258d10e2c282e14342d4c066810487
Roger Mähler <roger.mahler@hotmail.com>
2024-04-24 09:45:22 +0200

    Refactor SQL query in GeoPolygonCategoryCountSqlCompilerMatcher.cs

--- sead.query.test/Infrastructure/SQL/CategoryCountSqlCompiler/GeoPolygonCategoryCountSqlCompilerMatcher.cs
+++ sead.query.test/Infrastructure/SQL/CategoryCountSqlCompiler/GeoPolygonCategoryCountSqlCompilerMatcher.cs
@@ -4,8 +4,8 @@ namespace SQT.SQL.Matcher
     public class GeoPolygonCategoryCountSqlCompilerMatcher : CategoryCountSqlCompilerMatcher
     {
         public static string SqlRegExpr { get; } = $@"
-                SELECT (?<CategoryExpr>[\w\._]+) AS category, 1 as count, ([\w\._]+).longitude_dd, ([\w\._]+).latitude_dd
-                FROM (?<TargetSql>[\w\."",\(\)]+)(?: AS \w*)?(?<JoinSql>.*?(?= WHERE))?
+                SELECT [\w_]+.[\w_]+ AS category, 1 as count, COALESCE\([\w_]+.longitude_dd,0\), COALESCE\([\w_]+.latitude_dd,0\)
+                FROM (?<TargetSql>[\w\.,\(\)]+)(?: AS \w*)?(?<JoinSql>.*?(?= WHERE))?
                 WHERE(?<CriteriaSql>.*?(?= GROUP BY|\s?$))
                 GROUP BY [\w\._]+, 3, 4";
 
