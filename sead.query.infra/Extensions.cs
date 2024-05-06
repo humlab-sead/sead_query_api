80212f5933fcbb1914aa8902f374c98785b727fc
Roger Mähler <roger.mahler@hotmail.com>
2024-05-06 13:41:45 +0200

    Moved utility extension

--- sead.query.infra/Extensions.cs
+++ sead.query.infra/Extensions.cs
@@ -1,10 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.Common;
 using System.Linq;
 using System.Reflection;
+using NpgsqlTypes;
 
 namespace SeadQueryInfra
 {
+    public static class DataReaderExtension
+    {
+        public static (T, T) GetRange<T>(this DbDataReader dr, int index)
+        {
+            NpgsqlRange<T> range = dr.GetFieldValue<NpgsqlRange<T>>(index);
+            if (range.IsEmpty)
+                return (default(T), default(T));
+            return (range.LowerBound, range.UpperBound);
+        }
+    }
+
     public static class TypeLoaderExtensions
     {
         public static IEnumerable<Type> GetLoadableTypes(this Assembly assembly)
