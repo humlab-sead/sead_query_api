ec9817f955a58f767c0002bf047485c511b1137a
Roger Mähler <roger.mahler@hotmail.com>
2024-05-08 13:06:24 +0200

    refactor: Update FacetLoadService.cs to include missing test cases

--- sead.query.test/LiveTests/FacetLoadService.cs
+++ sead.query.test/LiveTests/FacetLoadService.cs
@@ -31,10 +31,11 @@ namespace SQT.LiveServices
             => ResultConfigFactory.Create(Registry.Facets.GetByCode(facetCode), Registry.Results.GetByKey(specificationKey), viewTypeId);
 
         [Theory]
-        // [InlineData("genus:genus")]
-        // [InlineData("abundance_classification:abundance_classification")]
+        [InlineData("genus:genus")]
+        [InlineData("abundance_classification:abundance_classification")]
         [InlineData("sites_polygon:sites_polygon@63.872484,20.093291,63.947006,20.501316,63.878949,20.673213,63.748021,20.252953,63.793983,20.095738")]
-        // [InlineData("sites_polygon:sites_polygon")]
+        [InlineData("sites_polygon:sites_polygon")]
+        [InlineData("sites:sites/tbl_denormalized_measured_values_33_0")]
         public void Load_VariousConfigs_Success(string uri)
         {
             var fakeFacetsConfig = FakeFacetsConfig(uri);
