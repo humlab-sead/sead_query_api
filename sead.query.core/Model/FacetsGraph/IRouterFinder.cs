68e0e86be4685805816ce5fc1836544128093317
Roger Mähler <roger.mahler@hotmail.com>
2024-06-10 15:53:42 +0200

    refactor: Renamed and simplified interface

---
+++ sead.query.core/Model/FacetsGraph/IRouterFinder.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SeadQueryCore
+{
+    public interface IRouteFinder
+    {
+
+        GraphRoute Find(string startTable, string destinationTable);
+        List<GraphRoute> Find(string startTable, List<string> destinationTable, bool reduce = true);
+
+    }
+}
