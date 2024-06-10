1b4f752da1e6fa41711e9f0eff3375740bfac80c
Roger Mähler <roger.mahler@hotmail.com>
2024-06-10 16:00:54 +0200

    refactor: renamed repository, add medthod

---
+++ sead.query.core/Interfaces/IRelationRepository.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace SeadQueryCore
+{
+    public interface IEdgeRepository : IRepository<TableRelation, int>
+    {
+        TableRelation FindByName(string sourceName, string targetName);
+        public IEnumerable<TableRelation> GetEdges(bool bidirectional = true);
+    }
+}
