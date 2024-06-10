db85c2732e052178100f7c8f855a2af8949e329f
Roger Mähler <roger.mahler@hotmail.com>
2024-06-10 21:21:25 +0200

    refactor: Add GetEdges method and ToRoute method to IEdgeRepository

--- sead.query.core/Interfaces/IRelationRepository.cs
+++ sead.query.core/Interfaces/IRelationRepository.cs
@@ -5,6 +5,7 @@ namespace SeadQueryCore
     public interface IEdgeRepository : IRepository<TableRelation, int>
     {
         TableRelation FindByName(string sourceName, string targetName);
-        public IEnumerable<TableRelation> GetEdges(bool bidirectional = true);
+        IEnumerable<TableRelation> GetEdges(bool bidirectional = true);
+        GraphRoute ToRoute(IEnumerable<int> trail);
     }
 }
