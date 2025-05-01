50f7a6d89b6f8b08ff798e64379e940e4abbcae3
Roger Mähler <roger.mahler@hotmail.com>
2025-05-01 11:19:57 +0200

    refactor: adapt to DebugTests to TestContainers.NET

--- sead.query.test/IntegrationTests/DebugTests.cs
+++ sead.query.test/IntegrationTests/DebugTests.cs
@@ -2,19 +2,14 @@ using IntegrationTests.Sead;
 using Newtonsoft.Json;
 using SeadQueryCore;
 using SeadQueryCore.Model;
-using SQT;
-using SQT.Infrastructure;
 using SQT.SQL.Matcher;
-using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
 using Xunit;
 using System.Text;
-using System.Collections.Generic;
 
 namespace IntegrationTests.Debug
 {
-    [Collection("SeadJsonFacetContextFixture")]
     public class DebugTests : ControllerTest<TestHostWithContainer>, IClassFixture<TestHostWithContainer>, IClassFixture<PostgresTestcontainerFixture>
     {
         readonly string jsonThatFails = @"{
@@ -45,13 +40,9 @@ namespace IntegrationTests.Debug
                     ]
                 }
             }";
-        public JsonFacetContextFixture FacetContextFixture { get; }
-        public DisposableFacetContextContainer MockService { get; }
 
-        public DebugTests(TestHostWithContainer hostBuilderFixture, SeadJsonFacetContextFixture facetContextFixture) : base(hostBuilderFixture)
+        public DebugTests(TestHostWithContainer hostBuilderFixture) : base(hostBuilderFixture)
         {
-            FacetContextFixture = facetContextFixture;
-            MockService = new DisposableFacetContextContainer(facetContextFixture);
         }
 
         protected StringContent FakeLoadResultPayload(FacetsConfig2 facetsConfig, ResultConfig resultConfig)
@@ -61,28 +52,13 @@ namespace IntegrationTests.Debug
             return payload;
         }
 
-        // [Fact]
-        // public void BuggTestByService()
-        // {
-        //     var data = JObject.Parse(jsonThatFails);
-        //     var facetsConfigDTO = data["facetsConfig"].ToObject<FacetsConfig2>();
-        //     var resultConfigDTO = data["resultConfig"].ToObject<ResultConfigDTO>();
-        //     var facetsConfig = new FacetConfigReconstituteService(MockService.Registry).Reconstitute(facetsConfigDTO);
-        //     var resultConfig = new ResultConfigReconstituteService(MockService.Registry).Reconstitute(resultConfigDTO);
-
-        //     IContainer container = DependencyService.CreateContainer(MockService.FacetContext, "PAP", MockService.Settings);
-
-        //     ILoadResultService service = container.Resolve<ILoadResultService>();
-        //     service.Load(facetsConfig, resultConfig).Nullify();
-        // }
 
         [Fact]
         public async Task BuggTest2()
         {
-                // /home/sead/supersead.humlab.umu.se/postgresql/mounts/pg-data-volume/pg_hba.conf
             var payload = new StringContent(jsonThatFails, Encoding.UTF8, "application/json");
             var response = await Fixture.Client.PostAsync("api/result/load", payload);
-
+        
             response.EnsureSuccessStatusCode();
 
             var responseContent = await response.Content.ReadAsStringAsync();
@@ -105,72 +81,5 @@ namespace IntegrationTests.Debug
             Assert.True(match.Success);
         }
 
-        [Fact]
-        public async Task BuggTest()
-        {
-            const string resultFacetCode = "map_result";
-            const string specificationKey = "site_level";
-            const string viewType = "map";
-
-            var facetsConfig2 = new FacetsConfig2
-            {
-                RequestId = "3",
-                RequestType = "populate",
-                DomainCode = "",
-                TargetCode = "ecocode",
-                FacetConfigs = new List<FacetConfig2>
-                {
-                    new FacetConfig2
-                    {
-                        FacetCode = "ecocode",
-                        Position = 1,
-                        TextFilter = "",
-                        Picks = new List<FacetConfigPick>
-                        {
-                            new FacetConfigPick { PickValue="36", Text="36"}
-                        }
-                    }
-                }
-            };
-
-            var facet = MockService.Facets.GetByCode(resultFacetCode);
-            var resultSpecification = MockService.Results.GetByKey(specificationKey);
-
-            var resultConfig = new ResultConfig()
-            {
-                Facet = facet,
-                FacetCode = facet.FacetCode,
-                RequestId = "1",
-                SessionId = "1",
-                Specifications = new List<ResultSpecification> { resultSpecification },
-                SpecificationKeys = new List<string> { specificationKey },
-                ViewTypeId = viewType
-            };
-
-            var payload = FakeLoadResultPayload(facetsConfig2, resultConfig);
-
-            var response = await Fixture.Client.PostAsync("api/result/load", payload);
-
-            response.EnsureSuccessStatusCode();
-
-            var responseContent = await response.Content.ReadAsStringAsync();
-
-            var result = JsonConvert.DeserializeObject<ResultContentSet>(responseContent);
-
-            Assert.NotNull(result);
-            Assert.NotNull(result.Data);
-            Assert.NotNull(result.Data.DataCollection);
-            Assert.NotNull(result.Meta);
-            Assert.NotNull(result.Meta.Columns);
-            Assert.True(result.Meta.Columns.Count > 0);
-            Assert.NotEmpty(result.Query);
-
-            var sqlQuery = result.Query.Squeeze();
-
-            var matcher = new MapResultSqlCompilerMatcher();
-            var match = matcher.Match(sqlQuery);
-
-            Assert.True(match.Success);
-        }
     }
 }
