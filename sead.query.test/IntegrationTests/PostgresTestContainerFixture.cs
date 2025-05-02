4e21b939d2f217ecbe3c176af606544f0318e2bb
Roger Mähler <roger.mahler@hotmail.com>
2025-05-02 14:12:38 +0200

    refactor: change PostgresTestcontainerFixture to singleton

--- sead.query.test/IntegrationTests/PostgresTestContainerFixture.cs
+++ sead.query.test/IntegrationTests/PostgresTestContainerFixture.cs
@@ -12,17 +12,22 @@ using SQT.Scaffolding;
 
 public class PostgresTestcontainerFixture : IAsyncLifetime
 {
-    //constructor
+    private static PostgreSqlTestcontainer _container;
+    private static bool                  _started;
     public PostgresTestcontainerFixture()
     {
         Options = SettingFactory.GetSettings();
     }
     
+    public PostgreSqlTestcontainer Container => _container;
     public ISetting Options { get; private set; }
     
-    public PostgreSqlTestcontainer Container { get; private set; }
     public string ConnectionString => Container.ConnectionString;
 
+    // FIXME: Use a Singleton pattern to avoid multiple initializations
+    // FIXME: Use a custom image with the database already set up (option)
+    // FIXME: Mount PGDATA to enable reuse (i.e. caching) of database
+    // FIXME: Use a random port to avoid conflicts
     public async Task InitializeDatabaseAsync(string connectionString, string sqlFilePath)
     {
         var sql = await File.ReadAllTextAsync(sqlFilePath);
@@ -36,22 +41,35 @@ public class PostgresTestcontainerFixture : IAsyncLifetime
 
     public async Task InitializeAsync()
     {
-        var config = new PostgreSqlTestcontainerConfiguration
+        if (!_started)
         {
-            Database = Options.Store.Database,
-            Username = Environment.GetEnvironmentVariable("QueryBuilderSetting__Store__Username"),
-            Password = Environment.GetEnvironmentVariable("QueryBuilderSetting__Store__Password"),
-            Port = int.Parse(Options.Store.Port)
-        };
+            var config = new PostgreSqlTestcontainerConfiguration
+            {
+                Database = Options.Store.Database,
+                Username = Environment.GetEnvironmentVariable("QueryBuilderSetting__Store__Username"),
+                Password = Environment.GetEnvironmentVariable("QueryBuilderSetting__Store__Password"),
+                Port = int.Parse(Options.Store.Port)
+            };
+            _container = new TestcontainersBuilder<PostgreSqlTestcontainer>()
+                .WithDatabase(config)
+                .WithImage("postgres:15-alpine")
+                .WithCleanUp(true)
+                // don't bind to a fixed host port — let it pick one
+                //.WithPortBinding(5432, assignRandomHostPort: true)
+                .Build();
 
-        Container = new TestcontainersBuilder<PostgreSqlTestcontainer>()
-            .WithDatabase(config)
-            .WithImage("postgres:15-alpine")
-            .WithCleanUp(true)
-            .Build();
+            await _container.StartAsync();
 
-        await Container.StartAsync();
+            await SetupDatabase();
 
+            _started = true;
+        }
+
+
+    }
+
+    private async Task SetupDatabase()
+    {
         var schemaFilePath = Path.Combine(ScaffoldUtility.GetDataFolder("PostgreSQL"), "initdb.d");
 
         foreach (var file in Directory.EnumerateFiles(schemaFilePath, "*.sql", SearchOption.TopDirectoryOnly))
@@ -60,8 +78,10 @@ public class PostgresTestcontainerFixture : IAsyncLifetime
         }
     }
 
-    public async Task DisposeAsync()
-    {
-        await Container.StopAsync();
-    }
+    public Task DisposeAsync() => Task.CompletedTask;
+
+    // public async Task DisposeAsync()
+    // {
+    //     await Container.StopAsync();
+    // }
 }
