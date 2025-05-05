7a05b5e17515631510a59c47b346e831eb9de799
Roger Mähler <roger.mahler@hotmail.com>
2025-05-05 16:54:09 +0200

    refactor: remove unused usings

--- sead.query.test/IntegrationTests/SeadControllers/SeadDependencyService.cs
+++ sead.query.test/IntegrationTests/SeadControllers/SeadDependencyService.cs
@@ -1,15 +1,11 @@
-﻿using SeadQueryInfra;
-using SQT.Infrastructure;
-using SQT.Mocks;
-using SQT.Scaffolding;
-using System.IO;
+﻿using SQT.Infrastructure;
 
 namespace IntegrationTests.Sead
 {
     public class FacetDependencyService : DependencyService
     {
         public FacetDependencyService() :
-            base(new JsonSeededFacetContextFactory().Create("Json"))
+            base(null)
         {
         }
     }
