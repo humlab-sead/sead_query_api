6fa2d5c885822dbfe05e2a2006156f3f0b2b2d8f
Roger Mähler <roger.mahler@hotmail.com>
2025-05-05 08:53:28 +0200

    chore: move JsonSeededFacetContextFactory to Sqlite

---
+++ sead.query.test/Infrastructure/Mocks/FacetContext/Sqlite/JsonSeededFacetContextFactory.cs
@@ -0,0 +1,39 @@
+﻿using AutoFixture;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using SeadQueryCore;
+using SeadQueryInfra;
+using SQT.Infrastructure;
+using SQT.Scaffolding;
+using System.Threading.Tasks;
+
+namespace SQT.Mocks;
+
+public class JsonSeededFacetContextFactory
+{
+    public virtual async Task<FacetContext> CreateAsync(DbContextOptions options, string jsonFolder) //, string modelSchemaFilename = null)
+    {
+        var fixture = new JsonFacetContextDataFixture(ScaffoldUtility.GetDataFolder(jsonFolder));
+        using (var context = new JsonSeededFacetContext(options, fixture))
+        {
+            await context.Database.EnsureCreatedAsync();
+            await context.SaveChangesAsync();
+            return context;
+        }
+    }
+
+    public virtual FacetContext Create(DbContextOptions options, string jsonFolder) //, string modelSchemaFilename = null)
+    {
+        return CreateAsync(options, jsonFolder).GetAwaiter().GetResult();
+    }
+
+    public virtual FacetContext CreateSqlite(string jsonFolder)
+    {
+        using (var connection = new SqliteConnection("DataSource=:memory:;Foreign Keys = False"))
+        {
+            connection.Open();
+            var options = new DbContextOptionsBuilder<FacetContext>().UseSqlite(connection).Options;
+            return Create(options, jsonFolder);
+        }
+    }
+}
