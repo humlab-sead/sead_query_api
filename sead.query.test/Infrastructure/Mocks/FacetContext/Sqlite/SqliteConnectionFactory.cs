523a9c6ce2ff46562f48d2fb48786599fc785d44
Roger Mähler <roger.mahler@hotmail.com>
2025-05-05 16:05:13 +0200

    refactor: refactor code to return both options and connection

--- sead.query.test/Infrastructure/Mocks/FacetContext/Sqlite/SqliteConnectionFactory.cs
+++ sead.query.test/Infrastructure/Mocks/FacetContext/Sqlite/SqliteConnectionFactory.cs
@@ -1,25 +1,65 @@
 ﻿using Microsoft.Data.Sqlite;
-using System;
-using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using SeadQueryInfra;
 using System.Data.Common;
-using System.Text;
+using System.Threading.Tasks;
 
 namespace SQT.Mocks
 {
-    internal static class SqliteConnectionFactory
+
+    // public class SqliteContextOptionsFactory
+    // {
+    //     public virtual DbContextOptions Create<T>(DbConnection connection=null) where T : DbContext
+    //     {
+    //         var builder = new DbContextOptionsBuilder<T>()
+    //             .UseSqlite(connection)
+    //             .EnableSensitiveDataLogging(true);
+
+    //         return builder.Options;
+    //     }
+    // }
+
+    internal class SqliteConnectionFactory
     {
-        public static DbConnection Create()
+        /// <summary>
+        /// Asynchronously build DbContextOptions by first creating/opening the connection.
+        /// </summary>
+        public virtual async Task<DbContextOptions> CreateDbContextOptionsAsync()
         {
-            var connection = new SqliteConnection("DataSource=:memory:;Foreign Keys = False");
-            connection.Open();
+            var (options, _) = await CreateDbContextOptionsAsync2();
+            return options;
+        }
+
+        public virtual async Task<(DbContextOptions, DbConnection)> CreateDbContextOptionsAsync2()
+        {
+            var connection = await CreateAsync().ConfigureAwait(false);
+            var builder = new DbContextOptionsBuilder<FacetContext>()
+                .UseSqlite(connection)
+                .EnableSensitiveDataLogging(true);
+            return (builder.Options, connection);
+        }
+
+        /// <summary>
+        /// Synchronous options builder (unchanged).
+        /// </summary>
+        public virtual DbContextOptions CreateDbContextOptions()
+            => CreateDbContextOptionsAsync().GetAwaiter().GetResult();
+
+        /// <summary>
+        /// Asynchronously create and open an in-memory SQLite connection.
+        /// </summary>
+        public virtual async Task<SqliteConnection> CreateAsync()
+        {
+            var connection = new SqliteConnection("DataSource=:memory:;Foreign Keys=False");
+            await connection.OpenAsync().ConfigureAwait(false);
             return connection;
         }
 
-        public static DbConnection CreateAndOpen()
-        {
-            var connection = Create();
-            connection.Open();
-            return connection;
-        }
+        /// <summary>
+        /// Alias for CreateAsync – returns a generic DbConnection.
+        /// </summary>
+        public virtual async Task<DbConnection> CreateAndOpenAsync()
+            => await CreateAsync();
+ 
     }
 }
