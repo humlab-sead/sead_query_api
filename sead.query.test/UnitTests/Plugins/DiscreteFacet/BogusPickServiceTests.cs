c974c94ae2a3367f98858c0f2f8bc76b2eda067a
Roger Mähler <roger.mahler@hotmail.com>
2025-05-05 15:57:27 +0200

    fix: use facet  collection

--- sead.query.test/UnitTests/Plugins/DiscreteFacet/BogusPickServiceTests.cs
+++ sead.query.test/UnitTests/Plugins/DiscreteFacet/BogusPickServiceTests.cs
@@ -3,10 +3,12 @@ using SeadQueryCore.Plugin.Discrete;
 using SeadQueryCore.QueryBuilder;
 using SQT.Infrastructure;
 using System.Collections.Generic;
+using Xunit;
 
 namespace SQT.Plugins.Discrete
 {
-    public class DeleteBogusPickServiceTests(JsonFacetContextDataFixture fixture) : MockerWithFacetContext(fixture)
+    [Collection("SqliteFacetContext")]
+    public class DeleteBogusPickServiceTests(SqliteFacetContext facetContext) : MockerWithFacetContext(facetContext)
     {
         protected virtual Mock<IValidPicksSqlCompiler> MockValidPickCompiler(string returnSql)
         {
