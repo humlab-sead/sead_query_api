172f62829536a648a1241d9b12c94329188edb47
Roger Mähler <roger.mahler@hotmail.com>
2025-05-07 14:01:55 +0200

    test: move StudyJsonCollectionFixture to purgatory

---
+++ sead.query.test/Purgatory/StudyDb/StudyJsonCollectionFixture.cs
@@ -0,0 +1,23 @@
+﻿using System.IO;
+using SQT.Infrastructure;
+using SQT.Scaffolding;
+using Xunit;
+
+namespace Deprecated.StudyDb
+{
+    public class StudyJsonFacetContextFixture : JsonFacetContextDataFixture
+    {
+        public StudyJsonFacetContextFixture() :
+            base("Data/StudyDb")
+        {
+        }
+    }
+
+    [CollectionDefinition("StudyJsonSeededFacetContext")]
+    public class StudyJsonCollectionFixture : ICollectionFixture<StudyJsonFacetContextFixture>
+    {
+        // This class has no code, and is never created. Its purpose is simply
+        // to be the place to apply [CollectionDefinition] and all the
+        // ICollectionFixture<> interfaces.
+    }
+}
