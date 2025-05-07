4b7ee47c818a24fa5adbe4b0ed9a2f8d8277d930
Roger Mähler <roger.mahler@hotmail.com>
2025-05-07 15:00:51 +0200

    test: commented out StudyDb tests

--- sead.query.test/Purgatory/StudyDb/StudyControllers/FacetsControllerTests.cs
+++ sead.query.test/Purgatory/StudyDb/StudyControllers/FacetsControllerTests.cs
@@ -1,96 +1,96 @@
-﻿using IntegrationTests;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
-using SeadQueryCore;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Xunit;
+﻿// using IntegrationTests;
+// using Newtonsoft.Json;
+// using Newtonsoft.Json.Linq;
+// using SeadQueryCore;
+// using System.Collections.Generic;
+// using System.Linq;
+// using System.Threading.Tasks;
+// using Xunit;
 
-namespace Deprecated.StudyDb
-{
-    public class FacetsControllerHostWithContainer : TestHostFixture<Startup<StudyDependencyService>>
-    {
-    }
+// namespace Deprecated.StudyDb
+// {
+//     public class FacetsControllerHostWithContainer : TestHostFixture<Startup<StudyDependencyService>>
+//     {
+//     }
 
-    public class FacetsControllerTests : ControllerTest<FacetsControllerHostWithContainer>, IClassFixture<FacetsControllerHostWithContainer>
-    {
-        public FacetsControllerTests(FacetsControllerHostWithContainer fixture) : base(fixture)
-        {
-        }
+//     public class FacetsControllerTests : ControllerTest<FacetsControllerHostWithContainer>, IClassFixture<FacetsControllerHostWithContainer>
+//     {
+//         public FacetsControllerTests(FacetsControllerHostWithContainer fixture) : base(fixture)
+//         {
+//         }
 
-        [Fact]
-        public async Task API_GET_Server_IsAwake()
-        {
-            using (var response = await Fixture.Client.GetAsync("api/facets"))
-            {
-                response.EnsureSuccessStatusCode();
-                Assert.NotEmpty(await response.Content.ReadAsStringAsync());
-            }
-        }
+//         [Fact(Skip="Pending delete or refactor of StudyDb")]
+//         public async Task API_GET_Server_IsAwake()
+//         {
+//             using (var response = await Fixture.Client.GetAsync("api/facets"))
+//             {
+//                 response.EnsureSuccessStatusCode();
+//                 Assert.NotEmpty(await response.Content.ReadAsStringAsync());
+//             }
+//         }
 
-        [Fact]
-        public async Task API_GET_Health_IsGood()
-        {
-            using var response = await Fixture.Client.GetAsync("api/values");
-            response.EnsureSuccessStatusCode();
-            var json = await response.Content.ReadAsStringAsync();
-            // Assert.Matches(@"\[""\d{4}"",""\d{2}""\]", json);
-            Assert.Equal(@"[""hello"",""world"",""!""]", json);
-        }
+//         [Fact(Skip="Pending delete or refactor of StudyDb")]
+//         public async Task API_GET_Health_IsGood()
+//         {
+//             using var response = await Fixture.Client.GetAsync("api/values");
+//             response.EnsureSuccessStatusCode();
+//             var json = await response.Content.ReadAsStringAsync();
+//             // Assert.Matches(@"\[""\d{4}"",""\d{2}""\]", json);
+//             Assert.Equal(@"[""hello"",""world"",""!""]", json);
+//         }
 
-        [Fact]
-        public async Task API_GET_Facets_Success()
-        {
-            using (var response = await Fixture.Client.GetAsync("api/facets"))
-            {
-                response.EnsureSuccessStatusCode();
-                var json = await response.Content.ReadAsStringAsync();
-                List<JObject> facets = JsonConvert.DeserializeObject<List<JObject>>(json);
-                Assert.NotEmpty(facets);
-            }
-        }
+//         [Fact(Skip="Pending delete or refactor of StudyDb")]
+//         public async Task API_GET_Facets_Success()
+//         {
+//             using (var response = await Fixture.Client.GetAsync("api/facets"))
+//             {
+//                 response.EnsureSuccessStatusCode();
+//                 var json = await response.Content.ReadAsStringAsync();
+//                 List<JObject> facets = JsonConvert.DeserializeObject<List<JObject>>(json);
+//                 Assert.NotEmpty(facets);
+//             }
+//         }
 
-        [Theory]
-        [InlineData(1)]
-        [InlineData(5)]
-        [InlineData(10)]
-        public async Task API_GET_Facets_ById_Success(int facetId)
-        {
-            using (var response = await Fixture.Client.GetAsync($"api/facets/{facetId}"))
-            {
-                response.EnsureSuccessStatusCode();
-                var json = await response.Content.ReadAsStringAsync();
-                Facet facet = JsonConvert.DeserializeObject<Facet>(json);
-                Assert.Equal(facetId, facet.FacetId);
-            }
-        }
+//         [Theory(Skip="Pending delete or refactor of StudyDb")]
+//         [InlineData(1)]
+//         [InlineData(5)]
+//         [InlineData(10)]
+//         public async Task API_GET_Facets_ById_Success(int facetId)
+//         {
+//             using (var response = await Fixture.Client.GetAsync($"api/facets/{facetId}"))
+//             {
+//                 response.EnsureSuccessStatusCode();
+//                 var json = await response.Content.ReadAsStringAsync();
+//                 Facet facet = JsonConvert.DeserializeObject<Facet>(json);
+//                 Assert.Equal(facetId, facet.FacetId);
+//             }
+//         }
 
-        [Fact]
-        public async Task API_GET_Facets_Domain_Success()
-        {
-            using (var response = await Fixture.Client.GetAsync("api/facets/domain"))
-            {
-                response.EnsureSuccessStatusCode();
-                var json = await response.Content.ReadAsStringAsync();
-                List<Facet> facets = JsonConvert.DeserializeObject<List<Facet>>(json);
-                Assert.NotEmpty(facets);
-                var facetCodes = facets.Select(x => x.FacetCode);
-                Assert.Contains("pollen", facetCodes);
-            }
-        }
+//         [Fact(Skip="Pending delete or refactor of StudyDb")]
+//         public async Task API_GET_Facets_Domain_Success()
+//         {
+//             using (var response = await Fixture.Client.GetAsync("api/facets/domain"))
+//             {
+//                 response.EnsureSuccessStatusCode();
+//                 var json = await response.Content.ReadAsStringAsync();
+//                 List<Facet> facets = JsonConvert.DeserializeObject<List<Facet>>(json);
+//                 Assert.NotEmpty(facets);
+//                 var facetCodes = facets.Select(x => x.FacetCode);
+//                 Assert.Contains("pollen", facetCodes);
+//             }
+//         }
 
-        [Theory]
-        [InlineData("isotope")]
-        public async Task API_GET_Facets_Domain_ById_Success(string facetCode)
-        {
-            using (var response = await Fixture.Client.GetAsync($"api/facets/domain/{facetCode}"))
-            {
-                response.EnsureSuccessStatusCode();
-                var json = await response.Content.ReadAsStringAsync();
-                List<Facet> facets = JsonConvert.DeserializeObject<List<Facet>>(json);
-                Assert.NotEmpty(facets);
-            }
-        }
-    }
-}
+//         [Theory(Skip="Pending delete or refactor of StudyDb")]
+//         [InlineData("isotope")]
+//         public async Task API_GET_Facets_Domain_ById_Success(string facetCode)
+//         {
+//             using (var response = await Fixture.Client.GetAsync($"api/facets/domain/{facetCode}"))
+//             {
+//                 response.EnsureSuccessStatusCode();
+//                 var json = await response.Content.ReadAsStringAsync();
+//                 List<Facet> facets = JsonConvert.DeserializeObject<List<Facet>>(json);
+//                 Assert.NotEmpty(facets);
+//             }
+//         }
+//     }
+// }
