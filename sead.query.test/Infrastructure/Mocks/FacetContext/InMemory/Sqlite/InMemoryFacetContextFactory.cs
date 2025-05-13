44646e8776daa7fa6d0815f093d561804537fc3e
Roger Mähler <roger.mahler@hotmail.com>
2025-05-13 15:52:52 +0200

    feat: add InMemoryFacetContextFactory for creating in-memory context instances

---
+++ sead.query.test/Infrastructure/Mocks/FacetContext/InMemory/Sqlite/InMemoryFacetContextFactory.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+using SQT.Scaffolding;
+
+namespace SQT.Mocks;
+
+public class InMemoryFacetContextFactory
+{
+    public virtual async Task<InMemoryFacetContext> CreateAsync(string folder, string schema)
+    {
+        var fixtureOptions = new InMemoryFacetContextOptions { Folder = folder, Schema = schema };
+        var fixture = new CsvFacetContextDataFixture();
+        var context = await InMemoryFacetContext.CreateAsync(fixtureOptions, fixture);
+        await context.Database.EnsureCreatedAsync();
+        await context.SaveChangesAsync();
+        return context;
+    }
+}
