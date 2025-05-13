6ffdf0e2777a88fea09fdbfc7fdd702896163b63
Roger Mähler <roger.mahler@hotmail.com>
2025-05-13 13:20:10 +0200

    refactor: reorganize using directives and improve method formatting in JsonSeededFacetContextFactory

--- sead.query.test/Infrastructure/Mocks/FacetContext/InMemory/Sqlite/JsonSeededFacetContextFactory.cs
+++ sead.query.test/Infrastructure/Mocks/FacetContext/InMemory/Sqlite/JsonSeededFacetContextFactory.cs
@@ -1,8 +1,8 @@
-﻿using SeadQueryInfra;
+﻿using System.Data.Common;
+using System.Threading.Tasks;
+using SeadQueryInfra;
 using SQT.Infrastructure;
 using SQT.Scaffolding;
-using System.Data.Common;
-using System.Threading.Tasks;
 
 namespace SQT.Mocks;
 
@@ -13,10 +13,16 @@ public class JsonSeededFacetContextFactory
         var (context, _, _) = await CreateTupleAsync(jsonFolder);
         return context;
     }
-    public virtual async Task<(InMemoryFacetContext, DbConnection, JsonFacetContextDataFixture)> CreateTupleAsync(string jsonFolder)
+
+    public virtual async Task<(
+        InMemoryFacetContext,
+        DbConnection,
+        JsonFacetContextDataFixture
+    )> CreateTupleAsync(string jsonFolder)
     {
         var fixture = new JsonFacetContextDataFixture(jsonFolder);
-        var (options, connection) = await new SqliteConnectionFactory().CreateDbContextOptionsAsync2<InMemoryFacetContext>();
+        var (options, connection) =
+            await new SqliteConnectionFactory().CreateDbContextOptionsAsync2<InMemoryFacetContext>();
         var context = new InMemoryFacetContext(options, fixture, connection);
         await context.Database.EnsureCreatedAsync();
         await context.SaveChangesAsync();
