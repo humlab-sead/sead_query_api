184c695b215e56115c90c241060b5599910d5141
Roger Mähler <roger.mahler@hotmail.com>
2026-05-27 13:57:29 +0200

    feat(infra): add query overhaul repositories

---
+++ sead.query.infra/Repository/AnchorRepository.cs
@@ -0,0 +1,6 @@
+﻿using SeadQueryCore;
+
+namespace SeadQueryInfra
+{
+    public class AnchorRepository(RepositoryRegistry registry) : Repository<Anchor, int>(registry), IAnchorRepository { }
+}
---
+++ sead.query.infra/Repository/RouteRepository.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using SeadQueryCore;
+
+namespace SeadQueryInfra
+{
+    public class RouteRepository(RepositoryRegistry registry) : Repository<Route, int>(registry), IRouteRepository
+    {
+        /// <summary>
+        /// Overide that include SourceTable, TargetTable and Steps when calling GetAll
+        /// </summary>
+        /// <returns></returns>
+        protected override IQueryable<Route> GetInclude(IQueryable<Route> query)
+        {
+            return query.Include(r => r.SourceTable).Include(r => r.TargetTable).Include(r => r.Steps);
+        }
+
+        private Dictionary<string, Route> _routeLookup = null!;
+
+        private Dictionary<string, Route> CreateLookup()
+        {
+            var routes = GetAll();
+            var lookup = routes?.ToDictionary(r => r.Name, r => r) ?? [];
+            foreach (var route in routes?.Where(r => !string.IsNullOrEmpty(r.Alias)) ?? [])
+            {
+                lookup.TryAdd(route.Alias!, route);
+            }
+            return lookup;
+        }
+
+        private Dictionary<string, Route> RouteLookup => _routeLookup ??= CreateLookup();
+
+        public Route GetRoute(string key)
+        {
+            RouteLookup.TryGetValue(key, out var route);
+            return route;
+        }
+
+        public bool HasRoute(string key)
+        {
+            return RouteLookup.ContainsKey(key);
+        }
+    }
+}
